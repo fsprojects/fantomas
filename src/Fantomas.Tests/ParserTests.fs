07b465bc10a9a2de88b005416f2fccc299436a03
Phan Anh Dung <phananhdung309@yahoo.com>
2013-02-16 19:52:43 +0100

    Create the first test cases

---
+++ src/Fantomas.Tests/ParserTests.fs
@@ -0,0 +1,15 @@
+﻿module Fantomas.Tests.ParserTests
+
+open NUnit.Framework
+open FsUnit
+
+open Fantomas.Ast
+open Fantomas.Parser
+
+[<Test>]
+let ``simple declarations``() =
+    "let x = 42" |> parseExps |> should equal [[Let (false,[(PVar "x", Lit (Int 42))],Lit Unit)]]
+    "let x = 42\n\
+     let y = 24" |>  parseExps |> should equal [[Let (false,[(PVar "x", Lit (Int 42))],Lit Unit)];
+                                                [Let (false,[(PVar "y", Lit (Int 24))],Lit Unit)]]
+
