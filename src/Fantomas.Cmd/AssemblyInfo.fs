69d333fb6ed8a459fbd95a94f2fafc3316ace3af
Phan Anh Dung <phananhdung309@yahoo.com>
2013-03-20 09:37:24 +0100

    Basic version of the command line

---
+++ src/Fantomas.Cmd/AssemblyInfo.fs
@@ -0,0 +1,37 @@
+﻿module internal Fantomas.Cmd.AssemblyInfo
+
+open System
+open System.Reflection
+open System.Resources
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+let [<Literal>] version = "0.3.0"
+    
+// Version information
+[<assembly: AssemblyVersion(version)>]
+[<assembly: AssemblyFileVersion(version)>]
+[<assembly: AssemblyInformationalVersion(version)>]
+
+// Assembly information
+[<assembly: AssemblyTitle("Fantomas")>]
+[<assembly: AssemblyDescription("A source code formatting library for F#.")>]
+[<assembly: NeutralResourcesLanguage("en-US")>]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyCopyright("Copyright © Anh-Dung Phan 2013")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// Only allow types derived from System.Exception to be thrown --
+// any other types should be automatically wrapped.
+[<assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)>]
+
+do()
--- src/Fantomas.Cmd/Program.fs
+++ src/Fantomas.Cmd/Program.fs
@@ -1,5 +1,12 @@
 ﻿module Fantomas.Cmd.Program
 
+open System
+open System.IO
+open Microsoft.FSharp.Text.Args
+
+open Fantomas.FormatConfig
+open Fantomas.CodeFormatter
+
 // Options:
 //  --encoding=<encoding>               Set the encoding, e.g. UTF-8. 
 //                                      If not set, defaults to the platform default encoding (currently UTF-8)
@@ -13,14 +20,101 @@
 // Preferences:
 //  -indentSpaceNum=[1-10]              Set number of spaces to use for indentation
 //  -longIdentLength=[10-100]           The length to start breaking an expression to multiple lines
-//  [+|-]semicolonAtEndOfLine           Enable/disable semicolons at the end of line (default = false)
+//  [+|-]semicolonAtEndOfLine           Enable/disable semicolons at the end of line (default = true)
 //  [+|-]spaceBeforeArgument            Enable/disable spaces before the first argument (default = false)
 //  [+|-]spaceBeforeColon               Enable/disable spaces before colons (default = true)
 //  [+|-]spaceAfterComma                Enable/disable spaces after commas (default = true)
 //  [+|-]spaceAfterSemiColon            Enable/disable spaces after semicolons (default = true)
 //  [+|-]indentOnTryWith                Enable/disable indentation on try/with block (default = false)
 
+let helpText = ""
+
+let indentText = "Set number of spaces to use for indentation. The value is between 1 and 10."
+
+let semicolonEOFText = "Enable/disable semicolons at the end of line (default = true)."
+let argumentText = "Enable/disable spaces before the first argument (default = false)."
+let colonText = "Enable/disable spaces before colons (default = true)."
+let commaText = "Enable/disable spaces after commas (default = true)."
+let semicolonText = "Enable/disable spaces after semicolons (default = true)."
+let indentOnTryWithText = "Enable/disable indentation on try/with block (default = false)."
+
+let time f =
+  let sw = System.Diagnostics.Stopwatch.StartNew()
+  let r = f()
+  sw.Stop()
+  printfn "Time taken: %O s" sw.Elapsed
+  r
+
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
+let main args = 
+    let help = ref false
+    let recurse = ref false
+    let force = ref false
+
+    let outputPath = ref None
+    let inputPath = ref None
+    
+    let indent = ref 4
+    
+    let semicolonAtEndOfLine = ref false
+    let spaceBeforeArgument = ref false
+    let spaceBeforeColon = ref true
+    let spaceAfterComma = ref true
+    let spaceAfterSemiColon = ref true
+    let indentOnTryWith = ref false
+
+    let options =
+        [| ArgInfo("--help", ArgType.Set help, helpText);
+           ArgInfo("--recursive", ArgType.Set recurse, "Process the input folder recursively.");
+           ArgInfo("--force", ArgType.Set force, "Force to return original string if parsing fails.");
+           ArgInfo("--out", ArgType.String(fun s -> outputPath := Some s), "Set the output path.");
+
+           ArgInfo("--indent", ArgType.Int(fun i -> indent := i), indentText);
+           
+           ArgInfo("-semicolonAtEndOfLine", ArgType.Unit(fun () -> semicolonAtEndOfLine := false), semicolonEOFText);
+           ArgInfo("+semicolonAtEndOfLine", ArgType.Unit(fun () -> semicolonAtEndOfLine := true), semicolonEOFText);
+           ArgInfo("-spaceBeforeArgument", ArgType.Unit(fun () -> spaceBeforeArgument := false), argumentText);
+           ArgInfo("+spaceBeforeArgument", ArgType.Unit(fun () -> spaceBeforeArgument := true), argumentText);
+           ArgInfo("-spaceBeforeColon", ArgType.Unit(fun () -> spaceBeforeColon := false), colonText);
+           ArgInfo("+spaceBeforeColon", ArgType.Unit(fun () -> spaceBeforeColon := true), colonText);
+           ArgInfo("-spaceAfterComma", ArgType.Unit(fun () -> spaceAfterComma := false), commaText);
+           ArgInfo("+spaceAfterComma", ArgType.Unit(fun () -> spaceAfterComma := true), commaText);
+           ArgInfo("-spaceAfterSemiColon", ArgType.Unit(fun () -> spaceAfterSemiColon := false), semicolonText);
+           ArgInfo("+spaceAfterSemiColon", ArgType.Unit(fun () -> spaceAfterSemiColon := true), semicolonText);
+           ArgInfo("-indentOnTryWith", ArgType.Unit(fun () -> indentOnTryWith := false), indentOnTryWithText);
+           ArgInfo("+indentOnTryWith", ArgType.Unit(fun () -> indentOnTryWith := true), indentOnTryWithText); |]
+
+    let parseInput s = 
+        match !inputPath with
+        | None -> inputPath := Some s
+        | Some _ -> printfn "Input path has already been specified."
+
+    ArgParser.Parse(options, parseInput, "Fantomas.Cmd <filename>")
+
+    // Assume that we get the input as a single file
+
+    if Option.isNone !outputPath then
+        outputPath := !inputPath 
+                      |> Option.map (fun s ->
+                            let name = Path.GetFileNameWithoutExtension(s) + "_out"
+                            let ext = Path.GetExtension(s)
+                            Path.Combine(Path.GetDirectoryName(s), name + ext))
+
+    let config = { FormatConfig.Default with 
+                    IndentSpaceNum = !indent;
+                    SemicolonAtEndOfLine = !semicolonAtEndOfLine; 
+                    SpaceBeforeArgument = !spaceBeforeArgument; 
+                    SpaceBeforeColon = !spaceBeforeColon;
+                    SpaceAfterComma = !spaceAfterComma; 
+                    SpaceAfterSemicolon = !spaceAfterSemiColon; 
+                    IndentOnTryWith = !indentOnTryWith }
+    
+    match !inputPath, !outputPath with
+    | Some s1, Some s2 -> 
+        time (fun () -> processSourceFile s1 s2 config)
+        Console.WriteLine("{0} has been written.", s2)
+    | _ -> ()
+
+    Console.WriteLine("Press any key to finish...")
+    Console.ReadKey() |> ignore
     0
