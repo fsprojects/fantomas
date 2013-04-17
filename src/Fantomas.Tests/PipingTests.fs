655f58502aaf204a4245c4b404c21f5380a9c9e1
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-04-17 16:03:39 +0200

    Adding a failing test with infix operators and pipes

---
+++ src/Fantomas.Tests/PipingTests.fs
@@ -0,0 +1,35 @@
+﻿module Fantomas.Tests.PipingTests
+
+open NUnit.Framework
+open FsUnit
+
+open Fantomas.FormatConfig
+open Fantomas.CodeFormatter
+
+let config = FormatConfig.Default
+let newline = System.Environment.NewLine
+
+let inline prepend s content = s + content
+let inline append s content = content + s
+
+// the current behavior results in a compile error since the |> is merged to the last line 
+[<Test>]
+let ``should keep the pipe after infix operator``() =
+    formatSourceString false """
+let f x =
+    someveryveryveryverylongexpression
+    <|> if someveryveryveryverylongexpression then someveryveryveryverylongexpression else someveryveryveryverylongexpression
+    <|> if someveryveryveryverylongexpression then someveryveryveryverylongexpression else someveryveryveryverylongexpression
+    |> f
+
+    """ config
+    |> should equal """let f x = 
+    someveryveryveryverylongexpression 
+    <|> if someveryveryveryverylongexpression
+        then someveryveryveryverylongexpression
+        else someveryveryveryverylongexpression 
+    <|> if someveryveryveryverylongexpression
+        then someveryveryveryverylongexpression
+        else someveryveryveryverylongexpression
+    |> f
+"""
