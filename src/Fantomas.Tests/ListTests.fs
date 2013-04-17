2ecee2929565488e60fae75b5e86bd34df0a5b81
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-04-17 15:45:40 +0200

    Failing tests for list comprehension. Don't change this automatically to do notation.

---
+++ src/Fantomas.Tests/ListTests.fs
@@ -0,0 +1,28 @@
+﻿module Fantomas.Tests.ListTests
+
+open NUnit.Framework
+open FsUnit
+
+open Fantomas.FormatConfig
+open Fantomas.CodeFormatter
+
+let config = FormatConfig.Default
+let newline = System.Environment.NewLine
+
+let inline prepend s content = s + content
+let inline append s content = content + s
+
+// the current behavior results in a compile error since the -> is replaced to do notation
+// but this is not valid in this case
+[<Test>]
+let ``should keep -> notation``() =
+    formatSourceString false """let environVars target =
+    [for e in Environment.GetEnvironmentVariables target ->
+        let e1 = e :?> Collections.DictionaryEntry
+        e1.Key, e1.Value]
+    """ config
+    |> should equal """let environVars target = 
+    [for e in Environment.GetEnvironmentVariables target ->
+        let e1 = e :?> Collections.DictionaryEntry
+        e1.Key, e1.Value]
+"""
