f5fa3bbee7fe3a59f285ccf2a4e5498c58574824
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-04-18 11:54:17 +0200

    Adding a failing test which shows comment deletion

---
+++ src/Fantomas.Tests/CommentTests.fs
@@ -0,0 +1,14 @@
+﻿module Fantomas.Tests.CommentTests
+
+open NUnit.Framework
+open FsUnit
+
+open Fantomas.CodeFormatter
+open Fantomas.Tests.TestHelper
+
+[<Test>]
+let ``should keep // comments after nowarn directives``() =
+    formatSourceString false """#nowarn "51" // address-of operator can occur in the code
+    """ config
+    |> should equal """#nowarn "51" // address-of operator can occur in the code
+"""
