c1c31a45665c47d73a014b4e7d41b5e4c7f8f560
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-04-18 09:21:47 +0200

    Adding 2 tests which shows missing parens around active patterns

---
+++ src/Fantomas.Tests/ActivePatternTests.fs
@@ -0,0 +1,30 @@
+﻿module Fantomas.Tests.ActivePatternTests
+
+open NUnit.Framework
+open FsUnit
+
+open Fantomas.CodeFormatter
+open Fantomas.Tests.TestHelper
+
+[<Test>]
+let ``should keep parens around active patterns``() =
+    formatSourceString false """let (|Boolean|_|) = Boolean.parse
+    """ config
+    |> should equal """let (|Boolean|_|) = Boolean.parse
+"""
+
+[<Test>]
+let ``should keep parens around active patterns in module``() =
+    formatSourceString false """module Interpreted =
+    let (|Match|_|) = (|Match|_|) RegexOptions.None
+    """ config
+    |> should equal """module Interpreted = 
+    let (|Match|_|) = (|Match|_|) RegexOptions.None
+"""
+
+[<Test>]
+let ``should keep parens around active patterns in inlined functions``() =
+    formatSourceString false """let inline (|Match|_|) x = tryMatchWithOptions x
+    """ config
+    |> should equal """let inline (|Match|_|) x = tryMatchWithOptions x
+"""
