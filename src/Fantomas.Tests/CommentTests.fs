1d8232fba1507452558973679d0b481ed13f235c
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-04-18 12:01:04 +0200

    Adding another failing test with comments

--- src/Fantomas.Tests/CommentTests.fs
+++ src/Fantomas.Tests/CommentTests.fs
@@ -12,3 +12,19 @@ let ``should keep // comments after nowarn directives``() =
     """ config
     |> should equal """#nowarn "51" // address-of operator can occur in the code
 """
+
+
+[<Test>]
+let ``should keep // comments before module definition``() =
+    formatSourceString false """﻿// The original idea for this typeprovider is from Ivan Towlson
+// some text
+module FSharpx.TypeProviders.VectorTypeProvider
+
+let x = 1
+    """ config
+    |> should equal """﻿// The original idea for this typeprovider is from Ivan Towlson
+// some text
+module FSharpx.TypeProviders.VectorTypeProvider
+
+let x = 1
+"""
