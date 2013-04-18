086bdcc1f1c1c06a3a1e8e35d8448f2fd9def8a3
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-04-18 09:30:59 +0200

    Adding a test which shows failing class formatting.

---
+++ src/Fantomas.Tests/ClassTests.fs
@@ -0,0 +1,18 @@
+﻿module Fantomas.Tests.ClassTests
+
+open NUnit.Framework
+open FsUnit
+
+open Fantomas.CodeFormatter
+open Fantomas.Tests.TestHelper
+
+
+// the current behavior results in a compile error since parens in "DGMLClass()" are moved to a wrong place
+[<Test>]
+let ``should keep parens in class definition in the right place``() =
+    formatSourceString false """type DGMLClass() = class   
+    let mutable currentState = System.String.Empty
+    """ config
+    |> should equal """type DGMLClass() = class   
+    let mutable currentState = System.String.Empty
+"""
