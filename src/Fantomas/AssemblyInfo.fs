46af555b382a5f44374e7c8ea27a17dc7f4fa487
Phan Anh Dung <phananhdung309@yahoo.com>
2013-05-31 11:08:21 +0200

    Publish v0.9.6

--- src/Fantomas/AssemblyInfo.fs
+++ src/Fantomas/AssemblyInfo.fs
@@ -6,7 +6,7 @@ open System.Resources
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
-let [<Literal>] version = "0.9.5"
+let [<Literal>] version = "0.9.6"
     
 // Version information
 [<assembly: AssemblyVersion(version)>]
