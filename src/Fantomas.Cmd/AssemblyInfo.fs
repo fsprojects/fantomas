cd700fd84508f1a2b58a86c5382b4ebe4f77f8b5
Phan Anh Dung <phananhdung309@yahoo.com>
2013-06-07 16:27:28 +0200

    Deploy v0.9.7

--- src/Fantomas.Cmd/AssemblyInfo.fs
+++ src/Fantomas.Cmd/AssemblyInfo.fs
@@ -6,7 +6,7 @@ open System.Resources
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
-let [<Literal>] version = "0.9.6"
+let [<Literal>] version = "0.9.7"
     
 // Version information
 [<assembly: AssemblyVersion(version)>]
