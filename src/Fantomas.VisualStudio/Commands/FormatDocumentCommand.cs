edce469e4fe0976ba30dc4b3dd4b10b5a9ee6afd
Phan Anh Dung <phananhdung309@yahoo.com>
2013-06-23 15:39:03 +0200

    [VS] Set scrollbar and caret positions after formatting documents
    The mechanism is not ideal but ok for now.
    
    We scale these positions linearly based on document lengths. It's better
    to filter out whitespace lines in the end to be more precise.

--- src/Fantomas.VisualStudio/Commands/FormatDocumentCommand.cs
+++ src/Fantomas.VisualStudio/Commands/FormatDocumentCommand.cs
@@ -8,6 +8,7 @@ using Hestia.FSharpCommands.Utils;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Formatting;
 
 namespace Hestia.FSharpCommands.Commands
 {
@@ -28,7 +29,30 @@ namespace Hestia.FSharpCommands.Commands
 
         protected override Action GetNewCaretPositionSetter() 
         {
-            return () => {};
+            var caretPos = TextView.Caret.Position.BufferPosition;
+            var caretLine = TextView.TextSnapshot.GetLineFromPosition(caretPos.Position);
+            int line = TextView.TextBuffer.CurrentSnapshot.GetLineNumberFromPosition(caretLine.Start);
+            int column = caretLine.End - caretLine.Start;
+
+            // Get start line of scroll bar
+            var scrollBarLine = TextView.TextViewLines.FirstOrDefault(l => l.VisibilityState != VisibilityState.Hidden);
+            int scrollBarPos = (scrollBarLine == null) ? 0 : TextView.TextBuffer.CurrentSnapshot.GetLineNumberFromPosition(scrollBarLine.Start);
+            int length = TextView.TextBuffer.CurrentSnapshot.Length;
+            
+            Action setter = () =>
+            {
+                // Scale caret positions in a linear way
+                int newLine = line * TextView.TextBuffer.CurrentSnapshot.Length / length;
+                var newCaretPos = TextView.TextBuffer.CurrentSnapshot.GetLineFromLineNumber(newLine).Start.Add(column);
+                var caretPoint = new VirtualSnapshotPoint(TextView.TextBuffer.CurrentSnapshot, newCaretPos);
+                TextView.Caret.MoveTo(caretPoint);
+
+                // Assume that the document scales in a linear way
+                int newScrollBarPos = scrollBarPos * TextView.TextBuffer.CurrentSnapshot.Length / length;
+                TextView.ViewScroller.ScrollViewportVerticallyByLines(ScrollDirection.Down, newScrollBarPos);
+            };
+
+            return setter;
         }
     }
 }
