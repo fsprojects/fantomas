9d3d0f8ccbdc3ab9a18d444fe94ad7a4b8bbbfc7
Gustavo Guerra <gustavo@codebeside.org>
2013-07-03 19:36:34 +0100

    Fix #55

--- src/Fantomas.Tests/TestHelpers.fs
+++ src/Fantomas.Tests/TestHelpers.fs
@@ -3,6 +3,7 @@
 open NUnit.Framework
 open FsUnit
 
+open System
 open Fantomas.FormatConfig
 open Fantomas.CodeFormatter
 
@@ -10,6 +11,12 @@ let config = FormatConfig.Default
 let newline = "\n"
 let formatSourceString fsi s config = (formatSourceString fsi s config).Replace("\r\n", "\n")
 let formatSelectionFromString fsi r s config = (formatSelectionFromString fsi r s config).Replace("\r\n", "\n")
+let equal x = 
+    let x = 
+        match box x with
+        | :? String as s -> s.Replace("\r\n", "\n") |> box
+        | x -> x
+    equal x
 
 let inline prepend s content = s + content
 let inline append s content = content + s
