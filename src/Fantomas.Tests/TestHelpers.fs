d64ddf4b029978c5fd20375026957d2a54a6218a
Gustavo Guerra <gustavo@codebeside.org>
2013-07-03 11:22:51 +0100

    Fixes #55

--- src/Fantomas.Tests/TestHelpers.fs
+++ src/Fantomas.Tests/TestHelpers.fs
@@ -9,6 +9,7 @@ open Fantomas.CodeFormatter
 let config = FormatConfig.Default
 let newline = "\n"
 let formatSourceString fsi s config = (formatSourceString fsi s config).Replace("\r\n", "\n")
+let formatSelectionFromString fsi r s config = (formatSelectionFromString fsi r s config).Replace("\r\n", "\n")
 
 let inline prepend s content = s + content
 let inline append s content = content + s
