1d44c62495a9b19e3ce36719582f5ddef87c9554
Jared Parsons <jaredpar@microsoft.com>
2013-07-04 12:55:36 -0700

    Restore caret position on undo / redo
    This change will wrap the format operations inside of an
    ITextUndoTransaction that tracks the caret position.  This will cause
    the caret position to be consistent across Visual Studio undo / redo
    operations.  Before the caret would jump wildly around because Visual
    Studio made no attempt to track it and just picked a set offset in the
    ITextBuffer.
    
    Note that this will restore the caret position to the exact position it
    was in before and after the format operation.  It is possible that for
    format selection you may want to put the caret at the start of the
    selection for undo.  If that is the case then you need to adjust the
    caret to the start of the selection before calling
    AddBeforeTextBufferUndoChangePrimitive.  There is no way to say "track
    position X" instead it will just track wherever the caret is when this
    call is made.
    
    closes #44

--- src/Fantomas.VisualStudio/Commands/CommandBase.cs
+++ src/Fantomas.VisualStudio/Commands/CommandBase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 
 namespace Hestia.FSharpCommands.Commands
@@ -12,6 +13,11 @@ namespace Hestia.FSharpCommands.Commands
         public IWpfTextView TextView { get; set; }
 
         public Services Services { get; set; }
+
+        public ITextBuffer TextBuffer
+        {
+            get { return TextView.TextBuffer; }
+        }
 
         public abstract void Execute();
     }
