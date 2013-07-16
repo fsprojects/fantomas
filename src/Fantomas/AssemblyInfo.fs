4e3f55f0bdc7306e1331e38da8fc6799e243cda4
Phan Anh Dung <phananhdung309@yahoo.com>
2013-07-16 08:46:59 +0200

    Publish a NuGet package depending on FSharp.Compiler.Editor

--- src/Fantomas/AssemblyInfo.fs
+++ src/Fantomas/AssemblyInfo.fs
@@ -6,7 +6,7 @@ open System.Resources
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
-let [<Literal>] version = "0.9.9.2"
+let [<Literal>] version = "1.0.0"
     
 // Version information
 [<assembly: AssemblyVersion(version)>]
