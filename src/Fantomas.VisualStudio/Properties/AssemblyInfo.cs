1ba222a4844757f23e0128927953776183943a60
Phan Anh Dung <phananhdung309@yahoo.com>
2013-07-16 09:12:09 +0200

    Upload VS extension v0.5.0

--- src/Fantomas.VisualStudio/Properties/AssemblyInfo.cs
+++ src/Fantomas.VisualStudio/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FSharpCommands")]
+[assembly: AssemblyTitle("Fantomas.VisualStudio")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FSharpCommands")]
+[assembly: AssemblyProduct("Fantomas.VisualStudio")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
