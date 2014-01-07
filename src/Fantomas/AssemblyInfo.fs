3616d3beb701d90ad40fb774a2063774a90b54d0
Phan Anh Dung <phananhdung309@yahoo.com>
2014-01-07 09:01:21 +0100

    Release NuGet package and VS extension 1.0.5

--- src/Fantomas/AssemblyInfo.fs
+++ src/Fantomas/AssemblyInfo.fs
@@ -6,7 +6,7 @@ open System.Resources
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
-let [<Literal>] version = "1.0.4"
+let [<Literal>] version = "1.0.5"
     
 // Version information
 [<assembly: AssemblyVersion(version)>]
