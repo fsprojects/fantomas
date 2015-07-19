445e339f4c8987dc090fc4f7c3cb044ca28ab609
dungpa <phananhdung309@yahoo.com>
2015-07-19 23:29:02 +0200

    Update build scripts to use Paket

--- src/Fantomas/CodeFormatter.fsx
+++ src/Fantomas/CodeFormatter.fsx
@@ -1,4 +1,4 @@
-﻿#r "../packages/FSharp.Compiler.Service.0.0.73/lib/net45/FSharp.Compiler.Service.dll"
+﻿#r "../../packages/FSharp.Compiler.Service/lib/net45/FSharp.Compiler.Service.dll"
 
 #load "Utils.fs"
 #load "TokenMatcher.fs"
