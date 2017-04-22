e9cc9db7839498fe381f6c85aa31cddf88d45393
Anh-Dung Phan <Anh-Dung.Phan@simcorp.com>
2017-04-22 23:02:01 +0200

    Publish NuGet package version 2.6.0

--- src/Fantomas/AssemblyInfo.fs
+++ src/Fantomas/AssemblyInfo.fs
@@ -6,9 +6,9 @@ open System.Runtime.CompilerServices
 [<assembly: AssemblyTitleAttribute("FantomasLib")>]
 [<assembly: AssemblyProductAttribute("Fantomas")>]
 [<assembly: AssemblyDescriptionAttribute("Source code formatter for F#")>]
-[<assembly: AssemblyVersionAttribute("2.5.0")>]
-[<assembly: AssemblyFileVersionAttribute("2.5.0")>]
+[<assembly: AssemblyVersionAttribute("2.6.0")>]
+[<assembly: AssemblyFileVersionAttribute("2.6.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.5.0"
+    let [<Literal>] Version = "2.6.0"
