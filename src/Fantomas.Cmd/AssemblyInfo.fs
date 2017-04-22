7a80a730e3fa0266a9e0c1a610c3abcca21302ff
Anh-Dung Phan <phananhdung309@yahoo.com>
2017-04-22 23:13:26 +0200

    Publish NuGet package version 2.6.1 (#210)
    * Publish NuGet package version 2.6.0
    
    * Another NuGet push

--- src/Fantomas.Cmd/AssemblyInfo.fs
+++ src/Fantomas.Cmd/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Fantomas")>]
 [<assembly: AssemblyProductAttribute("Fantomas")>]
 [<assembly: AssemblyDescriptionAttribute("Source code formatter for F#")>]
-[<assembly: AssemblyVersionAttribute("2.5.0")>]
-[<assembly: AssemblyFileVersionAttribute("2.5.0")>]
+[<assembly: AssemblyVersionAttribute("2.6.1")>]
+[<assembly: AssemblyFileVersionAttribute("2.6.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.5.0"
+    let [<Literal>] Version = "2.6.1"
