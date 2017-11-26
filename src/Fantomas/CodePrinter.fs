591d30f3d0c1687e9f02eae6f4572f3db2c64be7
jindraivanek <jindra.ivanek@gmail.com>
2017-11-26 15:35:48 +0100

    For complex patterns, remove parentheses only in "identificator only" cases.

--- src/Fantomas/CodePrinter.fs
+++ src/Fantomas/CodePrinter.fs
@@ -1070,7 +1070,8 @@ and genComplexPat astContext = function
     | CPAttrib(ats, sp) -> genOnelinerAttributes astContext ats +> genComplexPat astContext sp
 
 and genComplexPats astContext = function
-    | ComplexPats [c] -> genComplexPat astContext c
+    | ComplexPats [CPId _ as c]
+    | ComplexPats [CPSimpleId _ as c] -> genComplexPat astContext c
     | ComplexPats ps -> sepOpenT +> col sepComma ps (genComplexPat astContext) +> sepCloseT
     | ComplexTyped(ps, t) -> genComplexPats astContext ps +> sepColon +> genType astContext false t
 
