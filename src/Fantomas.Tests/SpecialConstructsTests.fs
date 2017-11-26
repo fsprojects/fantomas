cb76ad6ebf999ddf4acf37017c623aaef8183fdf
jindraivanek <jindra.ivanek@gmail.com>
2017-11-26 21:03:22 +0100

    Support for embedded IL.

---
+++ src/Fantomas.Tests/SpecialConstructsTests.fs
@@ -0,0 +1,16 @@
+﻿module Fantomas.Tests.SpecialConstructsTests
+
+open NUnit.Framework
+open FsUnit
+
+open Fantomas.CodeFormatter
+open Fantomas.Tests.TestHelper
+
+[<Test>]
+let ``embedded IL``() =
+    formatSourceString false """
+let inline private retype<'T, 'U> (x : 'T) : 'U = (# "" x : 'U #)""" config
+    |> prepend newline
+    |> should equal """
+let inline private retype<'T, 'U> (x : 'T) : 'U = (# "" x : 'U #)
+"""
