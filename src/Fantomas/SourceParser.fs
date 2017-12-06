af8c4b332bccf981bf9f55aa1e5530c4a4f8fdb8
jindraivanek <jindra.ivanek@gmail.com>
2017-12-06 22:44:14 +0100

    Optional else in ElIf. Fix #226

--- src/Fantomas/SourceParser.fs
+++ src/Fantomas/SourceParser.fs
@@ -786,7 +786,7 @@ let (|IfThenElse|_|) = function
 let rec (|ElIf|_|) = function
     | SynExpr.IfThenElse(e1, e2, Some(ElIf(es, e3)), _, _, r, _) ->
         Some((e1, e2, r)::es, e3)
-    | SynExpr.IfThenElse(e1, e2, Some e3, _, _, r, _) ->
+    | SynExpr.IfThenElse(e1, e2, e3, _, _, r, _) ->
         Some([(e1, e2, r)], e3)
     | _ -> None
 
