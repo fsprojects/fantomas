db7166d09e2fbdc3ff8a0bb1b862835eb7b72ec1
nojaf <florian.verdonck@outlook.com>
2018-03-28 00:10:41 +0200

    Update fake build script

--- build.fsx
+++ build.fsx
@@ -3,29 +3,32 @@
 // --------------------------------------------------------------------------------------
 
 #r @"packages/FAKE/tools/FakeLib.dll"
-open Fake 
-open Fake.Git
+open Fake
 open Fake.AssemblyInfoFile
 open Fake.ReleaseNotesHelper
 open System
 
-// setEnvironVar "MSBuild" (ProgramFilesX86 @@ @"\MSBuild\12.0\Bin\MSBuild.exe")
-setEnvironVar "MSBuild" (ProgramFilesX86 @@ @"Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe")
 
 // Git configuration (used for publishing documentation in gh-pages branch)
 // The profile where the project is posted 
 let gitHome = "https://github.com/dungpa"
 // The name of the project on GitHub
 let gitName = "fantomas"
-let cloneUrl = "git@github.com:dungpa/fantomas.git"
 
 // The name of the project 
 // (used by attributes in AssemblyInfo, name of a NuGet package and directory in 'src')
 let project = "Fantomas"
 
+let projectUrl = sprintf "%s/%s" gitHome gitName
+
 // Short summary of the project
 // (used as description in AssemblyInfo and as a short summary for NuGet package)
 let summary = "Source code formatter for F#"
+
+let copyright = "Copyright \169 2018"
+let iconUrl = "https://raw.githubusercontent.com/dungpa/fantomas/master/fantomas_logo.png"
+let licenceUrl = "https://github.com/dungpa/fantomas/blob/master/LICENSE.md"
+let configuration = "Release"
 
 // Longer description of the project
 // (used as a description for NuGet package; line breaks are automatically cleaned up)
@@ -38,14 +41,13 @@ Some common use cases include
 
 // List of author names (for NuGet package)
 let authors = [ "Anh-Dung Phan"; "Gustavo Guerra" ]
+let owner = "Anh-Dung Phan"
 // Tags for your project (for NuGet package)
 let tags = "F# fsharp formatting beautifier indentation indenter"
 
 // (<solutionFile>.sln is built during the building process)
-let solutionFile  = "fantomas"
-let testAssemblies = "src/**/bin/Release/*Tests*.dll"
-
-Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+let solutionFile  = "fantomas.core"
+// Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 let release = parseReleaseNotes (IO.File.ReadAllLines "RELEASE_NOTES.md")
 
 // --------------------------------------------------------------------------------------
@@ -62,73 +64,74 @@ Target "AssemblyInfo" (fun _ ->
         Attribute.Version release.AssemblyVersion
         Attribute.FileVersion release.AssemblyVersion ] 
 
-  CreateFSharpAssemblyInfo "src/Fantomas/AssemblyInfo.fs"
-      ( Attribute.InternalsVisibleTo "Fantomas.Tests" :: Attribute.Title "FantomasLib" :: shared)
+  CreateFSharpAssemblyInfo "src/Fantomas.Core/AssemblyInfo.fs"
+      ( Attribute.InternalsVisibleTo "Fantomas.Tests.Core" :: Attribute.Title "FantomasLib" :: shared)
 
-  CreateFSharpAssemblyInfo "src/Fantomas.Cmd/AssemblyInfo.fs"
+  CreateFSharpAssemblyInfo "src/Fantomas.Cmd.Core/AssemblyInfo.fs"
       (Attribute.Title "Fantomas" :: shared)
+)
+
+Target "ProjectVersion" (fun _ ->
+    let setProjectVersion project =
+        XMLHelper.XmlPoke ("src/"+project+"/"+project+".fsproj")
+            "Project/PropertyGroup/Version/text()" release.NugetVersion
+    setProjectVersion "Fantomas.Core"
+    setProjectVersion "Fantomas.Cmd.Core"
+    setProjectVersion "Fantomas.Tests.Core"
 )
 
 // --------------------------------------------------------------------------------------
 // Build library & test project
-
 Target "Build" (fun _ ->
-    // We would like to build only one solution
-    !! ("src/" + solutionFile + ".sln")
-    |> MSBuildRelease "" "Rebuild"
-    |> ignore
+    DotNetCli.Build (fun p ->
+        { p with 
+            Project = (sprintf "src/%s.sln" solutionFile) 
+            Configuration = configuration
+        }
+    )
 )
 
 Target "UnitTests" (fun _ ->
-    !! testAssemblies 
-    |> NUnit (fun p ->        
-          { p with
-              DisableShadowCopy = true
-              TimeOut = TimeSpan.FromMinutes 20.
-              ErrorOutputFile = @"C:\Temp\nunit-error.txt"
-              Framework = "4.5"
-              Domain = NUnitDomainModel.MultipleDomainModel
-              OutputFile = "TestResults.xml" })
+    DotNetCli.Test (fun p ->
+        { p with 
+            Project = "src/Fantomas.Tests.Core/Fantomas.Tests.Core.fsproj"
+            Configuration = configuration
+            AdditionalArgs = ["--test-adapter-path:. --logger:nunit;LogFilePath=../../TestResults.xml"]
+        }
+    ) 
 )
 
 // --------------------------------------------------------------------------------------
 // Build a NuGet package
 
-Target "NuGet" (fun _ ->
-    NuGet (fun p -> 
-        { p with   
-            Authors = authors
-            Project = project
-            Summary = summary
-            Description = description
-            Version = release.NugetVersion
-            ReleaseNotes = String.Join(Environment.NewLine, release.Notes)
-            Tags = tags
-            OutputPath = "src/Fantomas.Cmd/bin/Release"
-            AccessKey = getBuildParamOrDefault "nugetkey" ""
-            // Allow publishing from local build
-            Publish = isLocalBuild
-            Dependencies = [ "FSharp.Compiler.Service", GetPackageVersion "packages" "FSharp.Compiler.Service" ] })
-        (project + ".nuspec")
+Target "Pack" (fun _ ->
+    let pack project =
+        let packParameters =
+            [
+                "--no-build"
+                "--no-restore"
+                sprintf "/p:Title=\"%s\"" project
+                "/p:PackageVersion=" + release.NugetVersion
+                sprintf "/p:Authors=\"%s\"" (String.Join(" ", authors))
+                sprintf "/p:Owners=\"%s\"" owner
+                "/p:PackageRequireLicenseAcceptance=false"
+                sprintf "/p:Description=\"%s\"" description
+                sprintf "/p:Summary=\"%s\"" (description.Substring(0,100))
+                sprintf "/p:PackageReleaseNotes=\"%O\"" ((toLines release.Notes).Replace(",",""))
+                sprintf "/p:Copyright=\"%s\"" copyright
+                sprintf "/p:PackageTags=\"%s\"" tags
+                sprintf "/p:PackageProjectUrl=\"%s\"" projectUrl
+                sprintf "/p:PackageIconUrl=\"%s\"" iconUrl
+                sprintf "/p:PackageLicenseUrl=\"%s\"" licenceUrl
+            ] |> String.concat " "
+        "pack src/"+project+"/"+project+".fsproj -c "+ configuration + " -o ../../bin " + packParameters
+        |> DotNetCli.RunCommand id
+    
+    pack "Fantomas.Core"
+    pack "Fantomas.Cmd.Core"
 )
 
-Target "NuGetCLI" (fun _ ->
-    NuGet (fun p -> 
-        { p with   
-            Authors = authors
-            Project = sprintf "%sCLI" project
-            Summary = sprintf "%s (CLI tool)" summary 
-            Description = description
-            Version = release.NugetVersion
-            ReleaseNotes = String.Join(Environment.NewLine, release.Notes)
-            Tags = tags
-            OutputPath = "src/Fantomas.Cmd/bin/Release"
-            AccessKey = getBuildParamOrDefault "nugetkey" ""
-            // Allow publishing from local build
-            Publish = isLocalBuild
-            Dependencies = [] })
-        (project + "CLI.nuspec")
-)
+Target "Push" (fun _ -> Paket.Push (fun p -> { p with WorkingDir = "bin" }))
 
 // --------------------------------------------------------------------------------------
 // Run all targets by default. Invoke 'build <Target>' to override
@@ -137,10 +140,11 @@ Target "All" DoNothing
 
 "Clean"
   ==> "AssemblyInfo"
+  ==> "ProjectVersion"
   ==> "Build"
   ==> "UnitTests"
+  ==> "Pack"
   ==> "All"
-  ==> "NuGet"
-  ==> "NuGetCLI"
+  ==> "Push"
 
 RunTargetOrDefault "All"
