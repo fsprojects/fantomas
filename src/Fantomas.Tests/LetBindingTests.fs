e2b72138cdd31933273aaada8fa2803033506b93
Verdonck <SIDFLOV@sidmar.be>
2018-10-30 11:44:55 +0100

    Added fix for comment after in

--- src/Fantomas.Tests/LetBindingTests.fs
+++ src/Fantomas.Tests/LetBindingTests.fs
@@ -17,3 +17,19 @@ let ``let in should be preserved with PreserveEOL option``() =
     formatSourceString false "let x = 1 in ()" { config with PreserveEndOfLine = true }
     |> should equal """let x = 1 in ()
 """
+
+[<Test>]
+let ``temp`` () =
+    let codeSnippet = """
+let f () = 
+  let x = 1 in   // the "in" keyword is available in F#
+    let y = 2 in 
+      x + y
+"""
+
+    formatSourceString false codeSnippet config
+    |> should equal """let f() =
+    let x = 1   // the "in" keyword is available in F#
+    let y = 2 
+    x + y
+"""
