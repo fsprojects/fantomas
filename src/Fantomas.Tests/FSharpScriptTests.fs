443ed8a775240dbdf07a6ffe8d2cc81b8da3e52a
nojaf <florian.verdonck@outlook.com>
2019-07-11 16:06:18 +0200

    E2E test that verifies #380

--- src/Fantomas.Tests/FSharpScriptTests.fs
+++ src/Fantomas.Tests/FSharpScriptTests.fs
@@ -37,3 +37,31 @@ let ``e2e script test with keyword __source__directory__`` () =
     |> should equal """#I __SOURCE_DIRECTORY__
 #load ".paket/load/net471/main.group.fsx"
 """
+
+[<Test>]
+let ``fantomas removes module and namespace if it is only 1 word`` () =
+    let source = """namespace Shared
+
+type Counter = int
+"""
+
+    let file = Path.Combine(Path.GetTempPath(), System.Guid.NewGuid().ToString("N") + ".fsx")
+    File.WriteAllText(file, source)
+    
+    let fantomasConfig =
+        { FormatConfig.FormatConfig.Default with
+            StrictMode = true
+            IndentSpaceNum = 2
+            SpaceBeforeColon = false }
+    let formattedFiles = FakeHelpers.formatCode fantomasConfig [file]
+    
+    let formattedSource = File.ReadAllText(file)
+    List.length formattedFiles == 1
+    File.Delete(file)
+    
+    formattedSource
+    |> String.normalizeNewLine
+    |> should equal """namespace Shared
+
+type Counter = int
+"""
