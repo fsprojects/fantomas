08a86516099c00ad84db75910d51f133a8a9f7e1
nojaf <florian.verdonck@outlook.com>
2019-08-30 13:54:43 +0200

    fix: single clause with multiple SynPat.Or should be on a new line. #465

--- src/Fantomas/CodePrinter.fs
+++ src/Fantomas/CodePrinter.fs
@@ -980,7 +980,9 @@ and genExpr astContext synExpr =
     | TryWith(e, cs) -> 
         let prefix = !- "try " +> indent +> sepNln +> genExpr astContext e +> unindent ++ "with"
         match cs with
-        | [c] -> 
+        | [SynMatchClause.Clause(SynPat.Or(_,_,_),_,_,_,_) as c] ->
+            atCurrentColumn (prefix +> indentOnWith +> sepNln +> col sepNln cs (genClause astContext true) +> unindentOnWith)
+        | [c] ->
             atCurrentColumn (prefix +> sepSpace +> genClause astContext false c)
         | _ -> 
             atCurrentColumn (prefix +> indentOnWith +> sepNln +> col sepNln cs (genClause astContext true) +> unindentOnWith)
