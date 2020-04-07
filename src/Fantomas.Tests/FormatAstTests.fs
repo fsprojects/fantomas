aaecb842b8bb009319399ade222f31ed3f3de22b
Ayrat Hudaygulov <ayrat.hudaygulov@jet.com>
2020-04-07 12:39:47 +0100

    few more test cases

--- src/Fantomas.Tests/FormatAstTests.fs
+++ src/Fantomas.Tests/FormatAstTests.fs
@@ -72,17 +72,74 @@ let ``default implementations in abstract classes should be emited as override f
 type Foo =
     abstract foo: int
     default __.foo = 1
-""" 
+"""
+    let code = SourceOrigin.SourceString sourceCode
     let fileName = "/tmp.fsx"
     
     let ast =
-        CodeFormatter.ParseAsync(fileName, SourceOrigin.SourceString sourceCode, FakeHelpers.createParsingOptionsFromFile fileName, sharedChecker.Value)
+        CodeFormatter.ParseAsync(fileName, code, FakeHelpers.createParsingOptionsFromFile fileName, sharedChecker.Value)
         |> Async.RunSynchronously
         |> Seq.head
         |> fst
 
     let formattedCode =
         CodeFormatter.FormatASTAsync(ast, fileName, [], None, config)
+        |> Async.RunSynchronously
+        |> String.normalizeNewLine
+
+    formattedCode
+    |> should equal """[<AbstractClass>]
+type Foo =
+    abstract foo: int
+    override __.foo = 1
+"""
+
+[<Test>]
+let ``default implementations in abstract classes with `default` keyword should be emited as it was before from AST with origin source, 742``() =
+    let sourceCode = """[<AbstractClass>]
+type Foo =
+    abstract foo: int
+    default __.foo = 1
+"""
+    let code = SourceOrigin.SourceString sourceCode
+    let fileName = "/tmp.fsx"
+    
+    let ast =
+        CodeFormatter.ParseAsync(fileName, code, FakeHelpers.createParsingOptionsFromFile fileName, sharedChecker.Value)
+        |> Async.RunSynchronously
+        |> Seq.head
+        |> fst
+
+    let formattedCode =
+        CodeFormatter.FormatASTAsync(ast, fileName, [], Some code, config)
+        |> Async.RunSynchronously
+        |> String.normalizeNewLine
+
+    formattedCode
+    |> should equal """[<AbstractClass>]
+type Foo =
+    abstract foo: int
+    default __.foo = 1
+"""
+
+[<Test>]
+let ``default implementations in abstract classes with `override` keyword should be emited as it was before from AST with origin source, 742``() =
+    let sourceCode = """[<AbstractClass>]
+type Foo =
+    abstract foo: int
+    override __.foo = 1
+"""
+    let code = SourceOrigin.SourceString sourceCode
+    let fileName = "/tmp.fsx"
+    
+    let ast =
+        CodeFormatter.ParseAsync(fileName, code, FakeHelpers.createParsingOptionsFromFile fileName, sharedChecker.Value)
+        |> Async.RunSynchronously
+        |> Seq.head
+        |> fst
+
+    let formattedCode =
+        CodeFormatter.FormatASTAsync(ast, fileName, [], Some code, config)
         |> Async.RunSynchronously
         |> String.normalizeNewLine
 
