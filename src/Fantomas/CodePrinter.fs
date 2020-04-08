1745a0259defa1be0c0445176ceaebcf6a7ccec4
Szer <szer.penguin@gmail.com>
2020-04-08 12:06:33 +0100

    Changed default printing of override members to `override` from `member` (#743)
    * changed default printing of override members to `override` from `member`
    
    * few more test cases
    
    * added info from token trivia to preserve member keyword
    
    * few more test cases
    
    * removed unnecessary list traverse
    
    * fixed failing test
    
    * removed unnecessary list creation
    
    * reverted to List.tryPick because it's faster
    https://gist.github.com/Szer/9f20f116dbc6ca2397af152ec673afba
    
    Co-authored-by: Ayrat Hudaygulov <ayrat.hudaygulov@jet.com>

--- src/Fantomas/CodePrinter.fs
+++ src/Fantomas/CodePrinter.fs
@@ -717,16 +717,25 @@ and genMemberFlagsForMemberBinding astContext (mf:MemberFlags) (rangeOfBindingAn
          | MFOverride _ ->
              (fun (ctx: Context) ->
                 ctx.Trivia
-                |> List.tryFind(fun { Type = t; Range = r }  -> t = MainNode "SynMemberDefn.Member" && RangeHelpers.``range contains`` r rangeOfBindingAndRhs)
+                |> List.tryFind(fun { Type = t; Range = r }  ->
+                    match t with
+                    | MainNode "SynMemberDefn.Member" -> // trying to get AST trivia
+                        RangeHelpers.``range contains`` r rangeOfBindingAndRhs
+                        
+                    | Token { TokenInfo = { TokenName = "MEMBER" } } -> // trying to get token trivia
+                        r.StartLine = rangeOfBindingAndRhs.StartLine
+                        
+                    | _ -> false
+                )
                 |> Option.bind(fun tn ->
                     tn.ContentBefore
-                    |> List.choose (fun tc ->
+                    |> List.tryPick (fun tc ->
                         match tc with
-                        | Keyword({ Content = kw }) when (kw = "override" || kw = "default") -> Some (!- (sprintf "%s " kw))
-                        | _ -> None)
-                    |> List.tryHead
+                        | Keyword({ Content = ("override" | "default" | "member") as kw }) -> Some (!- (kw + " "))
+                        | _ -> None
+                    )
                 )
-                |> Option.defaultValue (!- "member ")
+                |> Option.defaultValue (!- "override ")
                 <| ctx
              )
         <| ctx
