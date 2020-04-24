ab515f9d199aec85a53d435bda235f68e52d7c5e
nojaf <florian.verdonck@outlook.com>
2020-04-24 16:29:31 +0200

    Don't add newline before member definition. Fix #789

--- src/Fantomas.Tests/UnionTests.fs
+++ src/Fantomas.Tests/UnionTests.fs
@@ -56,6 +56,31 @@ type Type =
     | TyLam of Type * Type
     | TyVar of string
     | TyCon of string * Type list
+    override this.ToString() =
+        match this with
+        | TyLam (t1, t2) -> sprintf "(%s -> %s)" (t1.ToString()) (t2.ToString())
+        | TyVar a -> a
+        | TyCon (s, ts) -> s
+"""
+
+[<Test>]
+let ``newline between discriminated unions and members``() =
+    formatSourceString false """
+type Type
+    = TyLam of Type * Type
+    | TyVar of string
+    | TyCon of string * Type list
+    with override this.ToString() =
+            match this with
+            | TyLam (t1, t2) -> sprintf "(%s -> %s)" (t1.ToString()) (t2.ToString())
+            | TyVar a -> a
+            | TyCon (s, ts) -> s""" ({ config with NewlineBetweenTypeDefinitionAndMembers = true })
+    |> prepend newline
+    |> should equal """
+type Type =
+    | TyLam of Type * Type
+    | TyVar of string
+    | TyCon of string * Type list
 
     override this.ToString() =
         match this with
