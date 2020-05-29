2cc227a223c0ebbf875923f2687b0a1abf50041b
nojaf <florian.verdonck@outlook.com>
2020-05-29 08:59:15 +0200

    Renamed file in --check mode. Fixes #869

--- src/Fantomas.CoreGlobalTool.Tests/CheckTests.fs
+++ src/Fantomas.CoreGlobalTool.Tests/CheckTests.fs
@@ -36,3 +36,15 @@ let ``files that need formatting should report exit code 99``() =
     use fileFixture = new TemporaryFileCodeSample(NeedsFormatting)
     let (exitCode,_) = checkCode fileFixture.Filename
     exitCode |> should equal 99
+
+[<Test>]
+let ``check with Program.fs file`` () =
+    let codeSnippet = """[<EntryPoint>]
+let main _ = 0
+"""
+
+    use fileFixture =
+        new TemporaryFileCodeSample(codeSnippet, fileName = "Program")
+
+    let (exitCode, _) = checkCode fileFixture.Filename
+    exitCode |> should equal 0
