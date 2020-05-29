7ebd390ef3b0ed812f52b2e956e7dd4835a0511c
nojaf <florian.verdonck@outlook.com>
2020-05-29 17:46:27 +0200

    Fix path 😅

--- src/Fantomas.Benchmarks/Runners.fs
+++ src/Fantomas.Benchmarks/Runners.fs
@@ -13,7 +13,7 @@ let config = Fantomas.FormatConfig.FormatConfig.Default
 type CodePrinterTest() =
     [<Benchmark>]
     member _.Format() =
-        let path = "./paket-files/fsprojects/fantomas/src/Fantomas/CodePrinter.fs"
+        let path = __SOURCE_DIRECTORY__ + "/../../paket-files/fsprojects/fantomas/src/Fantomas/CodePrinter.fs"
         let fileName = Path.GetFileName(path)
         let parsingOptions = FakeHelpers.createParsingOptionsFromFile fileName
         let content = File.ReadAllText(path) |> SourceOrigin.SourceString
