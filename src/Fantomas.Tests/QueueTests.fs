232b91fe95244a49d269309c7d5d1f2bb233f33e
Jindřich Ivánek <jindra.ivanek@gmail.com>
2020-06-05 20:56:31 +0200

    test also for reversed queue

--- src/Fantomas.Tests/QueueTests.fs
+++ src/Fantomas.Tests/QueueTests.fs
@@ -1,25 +1,18 @@
 module Fantomas.Tests.QueueTests
 
+open FSharp.Compiler.AbstractIL.Internal
 open Fantomas
 open NUnit.Framework
 open FsUnit
 open Fantomas.Tests.TestHelper
 open FsCheck
 
-let private verboseConf =
-    { Config.Verbose with
-          MaxTest = 500
-          EndSize = 20
-          Runner = NUnitRunner() }
-
-
 [<Test>]
 let ``Queue.append``() =
-    Check.One
-        (verboseConf, (
-            fun xs ys ->
-                let q = Queue.ofList xs
-                let result = Queue.append q ys
-                let expected = (q, ys) ||> List.fold (fun q y -> Queue.conj y q)
-                Queue.toSeq result |> should equivalent (Queue.toSeq expected)    
-                ))
+    Check.One (Config.Default, fun xs ys ->
+        let q = Queue.ofList xs
+        let result q = Queue.append q ys |> Queue.toSeq |> Seq.toList
+        let expected q = (q, ys) ||> List.fold (fun q y -> Queue.conj y q) |> Queue.toSeq |> Seq.toList
+        result q |> should equivalent (expected q)
+        result (Queue.rev q) |> should equivalent (expected (Queue.rev q))
+        )
