9b37dc8d4f47f418bf6ec25e6050dfe24927dbd1
Jindřich Ivánek <jindra.ivanek@gmail.com>
2020-06-06 11:46:41 +0200

    Add Queue.append (#892)
    * Queue.append
    
    * Queue.append test
    
    * Fix proj file
    
    * test also for reversed queue
    
    * simplify

---
+++ src/Fantomas.Tests/QueueTests.fs
@@ -0,0 +1,18 @@
+module Fantomas.Tests.QueueTests
+
+open FSharp.Compiler.AbstractIL.Internal
+open Fantomas
+open NUnit.Framework
+open FsUnit
+open Fantomas.Tests.TestHelper
+open FsCheck
+
+[<Test>]
+let ``Queue.append``() =
+    Check.One (Config.Default, fun xs ys ->
+        let q = Queue.ofList xs
+        let result q = Queue.append q ys |> Queue.toSeq |> Seq.toList
+        let expected q = (q, ys) ||> List.fold (fun q y -> Queue.conj y q) |> Queue.toSeq |> Seq.toList
+        result q |> should equivalent (expected q)
+        result (Queue.rev q) |> should equivalent (expected (Queue.rev q))
+        )
