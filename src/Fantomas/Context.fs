f502c77f041ab51647d9f271932036d4de75de58
Jindřich Ivánek <jindra.ivanek@gmail.com>
2020-06-13 14:50:29 +0200

    Perf: short-circuit (+>) in ShortExpression mode (#919)
    * Shortcut (+>) when ConfirmedMultiline
    
    * clean

--- src/Fantomas/Context.fs
+++ src/Fantomas/Context.fs
@@ -304,7 +304,10 @@ let internal atCurrentColumnIndent (f : _ -> Context) (ctx : Context) =
 
 /// Function composition operator
 let internal (+>) (ctx : Context -> Context) (f : _ -> Context) x =
-    f (ctx x)
+    let y = ctx x
+    match y.WriterModel.Mode with
+    | ShortExpression infos when infos |> Seq.exists (fun x -> x.ConfirmedMultiline) -> y
+    | _ -> f y
 
 /// Break-line and append specified string
 let internal (++) (ctx : Context -> Context) (str : string) x =
