cfdb7e30cd813a85680158f5460fc732931f8962
nojaf <florian.verdonck@outlook.com>
2020-07-11 14:37:47 +0200

    Preserve with get in signature files. Fixes #945

--- src/Fantomas/CodePrinter.fs
+++ src/Fantomas/CodePrinter.fs
@@ -2405,6 +2405,10 @@ and genMemberSig astContext node =
     match node with
     | MSMember(Val(ats, px, ao, s, t, vi, _, ValTyparDecls(tds, _, tcs)), mf) ->
         let (FunType namedArgs) = (t, vi)
+        let isFunctionProperty =
+            match t with
+            | TFun _ -> true
+            | _ -> false
         let sepColonX =
             match tds with
             | [] -> sepColon
@@ -2417,6 +2421,7 @@ and genMemberSig astContext node =
                                    +> sepColonX
                                    +> genTypeList astContext namedArgs
                                    +> genConstraints astContext t
+                                   -- (genPropertyKind (not isFunctionProperty) mf.MemberKind)
                                    +> unindent)
 
     | MSInterface t -> !- "interface " +> genType astContext false t
