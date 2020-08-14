183ddf8f6038d736c86f841cac14e9a51fbc8dfa
nojaf <florian.verdonck@outlook.com>
2020-08-14 17:02:02 +0200

    Print trivia before closing brace. Fixes #977.

--- src/Fantomas/CodePrinter.fs
+++ src/Fantomas/CodePrinter.fs
@@ -1178,7 +1178,11 @@ and genExpr astContext synExpr =
             // The opening { of the CompExpr is being added at the App(_,_,Ident(_),CompExr(_)) level
             (expressionFitsOnRestOfLine
                 (genExpr astContext e +> sepCloseS)
-                (genExpr astContext e +> unindent +> sepNln +> sepCloseSFixed))
+                (genExpr astContext e
+                 +> unindent
+                 +> sepNln
+                 +> enterNodeTokenByName synExpr.Range "RBRACE"
+                 +> sepCloseSFixed))
 
     | CompExprBody(expr) ->
         let statements = collectComputationExpressionStatements expr
