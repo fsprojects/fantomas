dbb3c9eded147d4ce47d471c3785eb88b460a564
Florian Verdonck <florian.verdonck@outlook.com>
2020-08-19 18:21:05 +0200

    Print trivia after clause arrow and function keyword. Fixes #1010. (#1013)

--- src/Fantomas/CodePrinter.fs
+++ src/Fantomas/CodePrinter.fs
@@ -1293,7 +1293,10 @@ and genExpr astContext synExpr =
              +> col sepSpace sps (genSimplePats astContext)
              +> sepArrow
              +> autoIndentAndNlnIfExpressionExceedsPageWidth (genExpr astContext e))
-    | MatchLambda(sp, _) -> !- "function " +> colPre sepNln sepNln sp (genClause astContext true)
+    | MatchLambda(sp, _) ->
+        !- "function "
+        +> leaveNodeTokenByName synExpr.Range "FUNCTION"
+        +> colPre sepNln sepNln sp (genClause astContext true)
     | Match(e, cs) ->
         atCurrentColumn
             (!- "match "
@@ -2820,8 +2823,13 @@ and genClause astContext hasBar (Clause(p, e, eo) as node) =
     let clauseBody e (ctx: Context) =
         (autoIndentAndNlnIfExpressionExceedsPageWidth (genExpr astContext e)) ctx
 
+    let arrowRange = mkRange "arrowRange" p.Range.End e.Range.Start
     let pat = genPat astContext p
-    let body = optPre (!- " when ") sepNone eo (genExpr astContext) +> sepArrow +> clauseBody e
+    let body =
+        optPre (!- " when ") sepNone eo (genExpr astContext)
+        +> sepArrow
+        +> leaveNodeTokenByName arrowRange "RARROW"
+        +> clauseBody e
     genTriviaBeforeClausePipe p.Range +>
     ifElse hasBar (sepBar +> atCurrentColumnWithPrepend pat body) (pat +> body)
     |> genTrivia node.Range
