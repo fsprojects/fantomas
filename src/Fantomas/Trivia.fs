29f524e637d95a9d00ae3d9d24dd343e01a19237
Florian Verdonck <florian.verdonck@outlook.com>
2020-08-19 18:13:58 +0200

    Ignore SynExpr.CompExpr as TriviaNode candidate. Fixes #990. (#1005)

--- src/Fantomas/Trivia.fs
+++ src/Fantomas/Trivia.fs
@@ -35,6 +35,7 @@ let filterNodes nodes =
             "SynTypeDefnRepr.ObjectModel"
             "TypeDefnSig"
             "SynTypeDefnSigRepr.ObjectModel"
+            "SynExpr.CompExpr"
         ]
     nodes |> List.filter (fun (n: Node) -> not (Set.contains n.Type filterOutNodeTypes))
 
