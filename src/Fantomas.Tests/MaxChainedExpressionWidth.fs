7a6ac063696df6d33d0fde997342b0c271eef0ab
nojaf <florian.verdonck@outlook.com>
2020-08-21 16:54:57 +0200

    Further changes for MaxChainedExpressionWidth setting...

--- src/Fantomas.Tests/MaxChainedExpressionWidth.fs
+++ src/Fantomas.Tests/MaxChainedExpressionWidth.fs
@@ -16,3 +16,76 @@ let result =
         .UseSerilog(dispose = true)
         .UseStartup()
 """
+
+[<Test>]
+let ``generic parameter in chained expression`` () =
+    formatSourceString false """
+builder.CaptureStartupErrors(true).UseSerilog(dispose = true).UseStartup<Startup>()
+"""  config
+    |> prepend newline
+    |> should equal """
+builder
+    .CaptureStartupErrors(true)
+    .UseSerilog(dispose = true)
+    .UseStartup<Startup>()
+"""
+
+[<Test>]
+let ``aspnet core webhost builder sample`` () =
+    formatSourceString false """
+module Program
+
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Hosting
+open Serilog
+open Startup
+
+[<EntryPoint>]
+let main args =
+  Host
+    .CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(fun builder ->
+      builder
+        .CaptureStartupErrors(true)
+        .UseSerilog(dispose = true)
+        .UseStartup<Startup>()
+      |> ignore
+    )
+    .Build()
+    .Run()
+  0
+"""  config
+    |> prepend newline
+    |> should equal """
+module Program
+
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Hosting
+open Serilog
+open Startup
+
+[<EntryPoint>]
+let main args =
+    Host
+        .CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(fun builder ->
+        builder
+            .CaptureStartupErrors(true)
+            .UseSerilog(dispose = true)
+            .UseStartup<Startup>()
+        |> ignore)
+        .Build()
+        .Run()
+    0
+"""
+
+[<Test>]
+let ``short chained expression stays in one line`` () =
+    formatSourceString false """
+let foo = Host
+             .CreateDefaultBuilder(args)
+"""  config
+    |> prepend newline
+    |> should equal """
+let foo = Host.CreateDefaultBuilder(args)
+"""
