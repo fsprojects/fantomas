4b3ffc72cf3dabded032c1fbc78da7792353fc77
nojaf <florian.verdonck@outlook.com>
2020-09-01 12:38:19 +0200

    Removed debug logs

--- src/Fantomas.Extras/IgnoreFile.fs
+++ src/Fantomas.Extras/IgnoreFile.fs
@@ -23,10 +23,7 @@ module IgnoreFile =
             false
         else
             try
-                printfn ".fantomasignore at %s" (getIgnoreFilePath ())
                 let fullPath = Path.GetFullPath(file)
-                if isNull fullPath then
-                    failwithf "Path.GetFullPath is null for %s" file
                 ignores.Value.IsIgnored(fullPath, false)
             with
             | ex ->
