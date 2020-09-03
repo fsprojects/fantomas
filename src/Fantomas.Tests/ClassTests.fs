06118ee65b0672e0158fd40d1b37eb9cfb41b8d9
nojaf <florian.verdonck@outlook.com>
2020-09-03 19:25:10 +0200

    Further indent closing parenthesis for multiline string. Fixes #1055.

--- src/Fantomas.Tests/ClassTests.fs
+++ src/Fantomas.Tests/ClassTests.fs
@@ -551,3 +551,73 @@ let ``unit parameter to inherited class`` () =
 type Child() =
     inherit Parent()
 """
+
+[<Test>]
+let ``multiline string inside constructor, 1055`` () =
+    formatSourceString false "
+module Fantomas.CoreGlobalTool.Tests.ConfigTests
+
+open NUnit.Framework
+open FsUnit
+open Fantomas.CoreGlobalTool.Tests.TestHelpers
+
+[<Test>]
+let ``config file in working directory should not require relative prefix, 821`` () =
+    use fileFixture =
+        new TemporaryFileCodeSample(\"let a  = // foo
+                                                            9\")
+
+    use configFixture =
+        new ConfigurationFile(\"\"\"
+[*.fs]
+indent_size=2
+\"\"\"                                )
+
+    let (exitCode, output) =
+        runFantomasTool fileFixture.Filename
+
+    exitCode |> should equal 0
+    output
+    |> should startWith (sprintf \"Processing %s\" fileFixture.Filename)
+
+    let result = System.IO.File.ReadAllText(fileFixture.Filename)
+    result
+    |> should equal \"\"\"let a = // foo
+  9
+\"\"\"
+"      config
+    |> prepend newline
+    |> should equal "
+module Fantomas.CoreGlobalTool.Tests.ConfigTests
+
+open NUnit.Framework
+open FsUnit
+open Fantomas.CoreGlobalTool.Tests.TestHelpers
+
+[<Test>]
+let ``config file in working directory should not require relative prefix, 821`` () =
+    use fileFixture =
+        new TemporaryFileCodeSample(\"let a  = // foo
+                                                            9\")
+
+    use configFixture =
+        new ConfigurationFile(\"\"\"
+[*.fs]
+indent_size=2
+\"\"\"                            )
+
+    let (exitCode, output) = runFantomasTool fileFixture.Filename
+
+    exitCode |> should equal 0
+
+    output
+    |> should startWith (sprintf \"Processing %s\" fileFixture.Filename)
+
+    let result =
+        System.IO.File.ReadAllText(fileFixture.Filename)
+
+    result
+    |> should equal \"\"\"let a = // foo
+  9
+\"\"\"
+"
