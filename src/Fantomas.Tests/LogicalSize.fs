edfb723d38e3c863c8f9137b3f13139cd5d5c8b8
Matt Walker <matt.g.d.walker@gmail.com>
2020-09-19 19:54:09 -0400

    Add tests

---
+++ src/Fantomas.Tests/LogicalSize.fs
@@ -0,0 +1,103 @@
+module Fantomas.Tests.LogicalSize
+
+open NUnit.Framework
+open FsUnit
+open Fantomas.Tests.TestHelper
+open Fantomas.FormatConfig
+
+[<Test>]
+let ``logically sized lists are formatted properly`` () =
+    formatSourceString false """
+let xs = [ a; b; c ]
+let ys = [ AReallyLongExpressionThatIsMuchLongerThan50Characters ]
+f xs [ x; y; z ]
+    """ { config with ArrayOrListMultilineFormatter = LogicalSize }
+    |> prepend newline
+    |> should equal """
+let xs =
+    [ a
+      b
+      c ]
+
+let ys =
+    [ AReallyLongExpressionThatIsMuchLongerThan50Characters ]
+
+f
+    xs
+    [ x
+      y
+      z ]
+"""
+
+[<Test>]
+let ``logically sized arrays are formatted properly`` () =
+    formatSourceString false """
+let xs = [| a; b; c |]
+let ys = [| AReallyLongExpressionThatIsMuchLongerThan50Characters |]
+f xs [| x; y; z |]
+List.map (fun x -> x * x) [1;2;]
+    """ { config with ArrayOrListMultilineFormatter = LogicalSize }
+    |> prepend newline
+    |> should equal """
+let xs =
+    [| a
+       b
+       c |]
+
+let ys =
+    [| AReallyLongExpressionThatIsMuchLongerThan50Characters |]
+
+f
+    xs
+    [| x
+       y
+       z |]
+
+List.map (fun x -> x * x)
+    [ 1
+      2 ]
+"""
+
+[<Test>]
+let ``logically sized Elmish lists are formatted properly`` () =
+    formatSourceString false """
+f [ a; b; c ] [ x; y; z ]
+g [ longValueThatIsALotOfCharactersSoooooLong ] [ longValueThatIsALotOfCharactersSoooooLong ]
+h [ longValueThatIsALotOfCharactersSoooooLong; longValueThatIsALotOfCharactersSoooooLong ] [ longValueThatIsALotOfCharactersSoooooLong ]
+    """ { config with ArrayOrListMultilineFormatter = LogicalSize }
+    |> prepend newline
+    |> should equal """
+f [ a
+    b
+    c ] [
+    x
+    y
+    z
+]
+
+g [ longValueThatIsALotOfCharactersSoooooLong ] [
+    longValueThatIsALotOfCharactersSoooooLong
+]
+
+h [ longValueThatIsALotOfCharactersSoooooLong
+    longValueThatIsALotOfCharactersSoooooLong ] [
+    longValueThatIsALotOfCharactersSoooooLong
+]
+"""
+
+[<Test>]
+let ``character width sized Elmish lists are formatted properly`` () =
+    formatSourceString false """
+f [ a; b; c ] [ x; y; z ]
+g [ longValueThatIsALotOfCharactersSoooooLong; longValueThatIsALotOfCharactersSoooooLong ] [ x ]
+    """ config
+    |> prepend newline
+    |> should equal """
+f [ a; b; c ] [ x; y; z ]
+
+g [ longValueThatIsALotOfCharactersSoooooLong
+    longValueThatIsALotOfCharactersSoooooLong ] [
+    x
+]
+"""
+
