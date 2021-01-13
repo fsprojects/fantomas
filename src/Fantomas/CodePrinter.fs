6c325838a6efc3af3601f8942cb3f821df1ce3eb
Mike Bottazzo <1796534+btzo@users.noreply.github.com>
2021-01-13 04:16:04 -0300

    add genTrivia in genParsedHashDirective 1220 (#1360)
    * add genTrivia in genParsedHashDirective
    
    * call genTrivia for the entire expression
    
    Co-authored-by: Mike Bottazzo <mikebottazzo@gmail.com>

--- src/Fantomas/CodePrinter.fs
+++ src/Fantomas/CodePrinter.fs
@@ -128,6 +128,7 @@ and genParsedHashDirective (ParsedHashDirective (h, s, r)) =
         <| ctx
 
     !- "#" -- h +> sepSpace +> printIdent
+    |> genTriviaFor ParsedHashDirective_ r
 
 and genModuleOrNamespace astContext (ModuleOrNamespace (ats, px, ao, s, mds, isRecursive, moduleKind) as node) =
     let sepModuleAndFirstDecl =
