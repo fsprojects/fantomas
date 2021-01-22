926c6b61a81eb0b144cd16c3514d3978d47ed054
Florian Verdonck <florian.verdonck@outlook.com>
2021-01-22 16:51:42 +0100

    Bump Fantomas to Fantomas.Extras 4.4.0-beta-001 (#1384)

--- build.fsx
+++ build.fsx
@@ -1,5 +1,5 @@
 #r "paket:
-nuget Fantomas.Extras 4.4.0-alpha-008
+nuget Fantomas.Extras 4.4.0-beta-001
 nuget Microsoft.Azure.Cosmos.Table
 nuget Fake.BuildServer.AppVeyor
 nuget Fake.Core.ReleaseNotes
