1f9ca784062e8e110c65b85a300dff492d46b034
Florian Verdonck <florian.verdonck@outlook.com>
2021-10-20 19:33:13 +0200

    Format out of process (#1845)
    * Style guide lambda (#1858)
    
    * WIP lambda as last argument
    
    * Update remaining test according to style proposal.
    
    * Add test for multiline non lambda argument.
    
    * Print trivia for function keyword from ast range.
    
    * multiline non lambda argument, match lambda
    
    * WIP Daemon service contract.
    
    * Early working daemon prototype.
    
    * Take .editorconfig settings into account.
    
    * Fix current build.
    
    * Add Fantomas locator.
    
    * Found globally installed Fantomas version.
    
    * Remove unused function.
    
    * Add formatSelection.
    
    * Refactor FormatDocumentResponse to DU.
    
    * Remove unused LspTypes dependency.
    
    * Update FCS in Daemon
    
    * Refactored public api of FantomasService.
    
    * Don't pass full exception in FormatDocumentResponse.
    
    * Use separate versioning for Fantomas.Client.
    
    * FantomasService manages multiple daemon versions.
    
    * Fully implement FantomasService.
    
    * Return configuration as json.
    
    * Validate if filePath in request is absolute.
    
    * Ignore first three 4.6 alphas as compatible tools.
    
    * Find ignore file from current file path.
    
    * Remove safeFileName from CodeFormatterImpl.fs.
    Disable daemon tests.
    
    * Add daemon unit tests.
    
    * Update help text for daemon mode.
    
    * Add documentation for Fantomas daemon mode.
    
    * Initial attempt at adding source links.
    
    * Set DebugType to embedded.
    
    * Bump Fantomas.Client to 0.3.0

---
+++ src/Fantomas.CoreGlobalTool/Daemon.fs
@@ -0,0 +1,158 @@
+﻿module Fantomas.CoreGlobalTool.Daemon
+
+open System
+open System.Diagnostics
+open System.IO
+open System.Threading
+open System.Threading.Tasks
+open FSharp.Compiler.CodeAnalysis
+open FSharp.Compiler.Text.Range
+open FSharp.Compiler.Text.Position
+open StreamJsonRpc
+open Thoth.Json.Net
+open Fantomas.Client.Contracts
+open Fantomas.Client.LSPFantomasServiceTypes
+open Fantomas
+open Fantomas.SourceOrigin
+open Fantomas.FormatConfig
+open Fantomas.Extras.EditorConfig
+
+let private createParsingOptionsFromFile (fileName: string) : FSharpParsingOptions =
+    { FSharpParsingOptions.Default with
+          SourceFiles = [| fileName |]
+          IsExe = true }
+
+type FantomasDaemon(sender: Stream, reader: Stream) as this =
+    let rpc: JsonRpc = JsonRpc.Attach(sender, reader, this)
+
+    do
+        // hook up request/response logging for debugging
+        rpc.TraceSource <- TraceSource(typeof<FantomasDaemon>.Name, SourceLevels.Verbose)
+
+        rpc.TraceSource.Listeners.Add(new SerilogTraceListener.SerilogTraceListener(typeof<FantomasDaemon>.Name))
+        |> ignore<int>
+
+    let disconnectEvent = new ManualResetEvent(false)
+
+    let exit () = disconnectEvent.Set() |> ignore
+
+    do rpc.Disconnected.Add(fun _ -> exit ())
+
+    interface IDisposable with
+        member this.Dispose() = disconnectEvent.Dispose()
+
+    /// returns a hot task that resolves when the stream has terminated
+    member this.WaitForClose = rpc.Completion
+
+    [<JsonRpcMethod(Methods.Version)>]
+    member _.Version() : string = CodeFormatter.GetVersion()
+
+    [<JsonRpcMethod(Methods.FormatDocument, UseSingleObjectParameterDeserialization = true)>]
+    member _.FormatDocumentAsync(request: FormatDocumentRequest) : Task<FormatDocumentResponse> =
+        async {
+            if Fantomas.Extras.IgnoreFile.isIgnoredFile request.FilePath then
+                return FormatDocumentResponse.IgnoredFile request.FilePath
+            else
+                let config =
+                    match request.Config with
+                    | Some configProperties -> parseOptionsFromEditorConfig configProperties
+                    | None -> readConfiguration request.FilePath
+
+                try
+                    let! formatted =
+                        CodeFormatter.FormatDocumentAsync(
+                            request.FilePath,
+                            SourceString request.SourceCode,
+                            config,
+                            createParsingOptionsFromFile request.FilePath,
+                            CodeFormatterImpl.sharedChecker.Value
+                        )
+
+                    if formatted = request.SourceCode then
+                        return FormatDocumentResponse.Unchanged request.FilePath
+                    else
+                        return FormatDocumentResponse.Formatted(request.FilePath, formatted)
+                with
+                | ex -> return FormatDocumentResponse.Error(request.FilePath, ex.Message)
+        }
+        |> Async.StartAsTask
+
+    [<JsonRpcMethod(Methods.FormatSelection, UseSingleObjectParameterDeserialization = true)>]
+    member _.FormatSelectionAsync(request: FormatSelectionRequest) : Task<FormatSelectionResponse> =
+        async {
+            let config =
+                match request.Config with
+                | Some configProperties -> parseOptionsFromEditorConfig configProperties
+                | None -> readConfiguration request.FilePath
+
+            let range =
+                let r = request.Range
+                mkRange request.FilePath (mkPos r.StartLine r.StartColumn) (mkPos r.EndLine r.EndColumn)
+
+            try
+                let! formatted =
+                    CodeFormatter.FormatSelectionAsync(
+                        request.FilePath,
+                        range,
+                        SourceString request.SourceCode,
+                        config,
+                        CodeFormatterImpl.createParsingOptionsFromFile request.FilePath,
+                        CodeFormatterImpl.sharedChecker.Value
+                    )
+
+                return FormatSelectionResponse.Formatted(request.FilePath, formatted)
+            with
+            | ex -> return FormatSelectionResponse.Error(request.FilePath, ex.Message)
+        }
+        |> Async.StartAsTask
+
+    [<JsonRpcMethod(Methods.Configuration)>]
+    member _.Configuration() : string =
+        let settings =
+            Reflection.getRecordFields FormatConfig.FormatConfig.Default
+            |> Array.toList
+            |> List.choose
+                (fun (name, defaultValue) ->
+                    let type' =
+                        match defaultValue with
+                        | :? bool as b ->
+                            Some(
+                                Encode.object [ "type", Encode.string "boolean"
+                                                "defaultValue", Encode.string (if b then "true" else "false") ]
+                            )
+                        | :? int as i ->
+                            Some(
+                                Encode.object [ "type", Encode.string "number"
+                                                "defaultValue", Encode.string (string i) ]
+                            )
+                        | :? MultilineFormatterType as m ->
+                            Some(
+                                Encode.object [ "type", Encode.string "multilineFormatterType"
+                                                "defaultValue", Encode.string (MultilineFormatterType.ToConfigString m) ]
+                            )
+                        | :? EndOfLineStyle as e ->
+                            Some(
+                                Encode.object [ "type", Encode.string "endOfLineStyle"
+                                                "defaultValue", Encode.string (EndOfLineStyle.ToConfigString e) ]
+                            )
+                        | _ -> None
+
+                    type'
+                    |> Option.map (fun t -> toEditorConfigName name, t))
+            |> Encode.object
+
+        let enumOptions =
+            Encode.object [ "multilineFormatterType",
+                            Encode.list [ (MultilineFormatterType.ToConfigString MultilineFormatterType.CharacterWidth
+                                           |> Encode.string)
+                                          (MultilineFormatterType.ToConfigString MultilineFormatterType.NumberOfItems
+                                           |> Encode.string) ]
+                            "endOfLineStyle",
+                            Encode.list [ (EndOfLineStyle.ToConfigString EndOfLineStyle.LF
+                                           |> Encode.string)
+                                          (EndOfLineStyle.ToConfigString EndOfLineStyle.CRLF
+                                           |> Encode.string) ] ]
+
+        Encode.object [ "settings", settings
+                        "enumOptions", enumOptions ]
+        |> Encode.toString 4
