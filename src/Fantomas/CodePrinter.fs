c419411afea9f11f5cec603badeb3ddaf6355c5e
Florian Verdonck <florian.verdonck@outlook.com>
2021-10-22 11:56:11 +0200

    Don't add a newline if there are no additional arguments before lambda. (#1923)
    Fixes #1922.

--- src/Fantomas/CodePrinter.fs
+++ src/Fantomas/CodePrinter.fs
@@ -2211,7 +2211,7 @@ and genExpr astContext synExpr ctx =
                             +> indent
                             +> sepNln
                             +> col sepNln es (genExpr astContext)
-                            +> sepNln
+                            +> onlyIfNot (List.isEmpty es) sepNln
                             +> (sepOpenTFor lpr
                                 +> (!- "fun "
                                     +> col sepSpace pats (genPat astContext)
