272418152b922f4fb119897b2e9a3443081cb9b8
nojaf <florian.verdonck@outlook.com>
2022-02-15 18:55:24 +0100

    Initial version of Fantomas FCS.

---
+++ src/Fantomas.FCS/Lex.fs
@@ -0,0 +1,68 @@
+﻿module internal Fantomas.FCS.Lex
+
+open System
+open FSharp.Compiler
+open FSharp.Compiler.ErrorLogger
+open FSharp.Compiler.Lexhelp
+open FSharp.Compiler.ParseHelpers
+open FSharp.Compiler.Text
+
+[<Flags>]
+type FSharpLexerFlags =
+    | Default = 0x11011
+    | LightSyntaxOn = 0x00001
+    | Compiling = 0x00010
+    | CompilingFSharpCore = 0x00110
+    | SkipTrivia = 0x01000
+    | UseLexFilter = 0x10000
+
+let lex
+    (text: ISourceText)
+    (conditionalCompilationDefines: string list)
+    (onToken: Parser.token -> range -> unit)
+    (pathMap: Internal.Utilities.PathMap)
+    : unit =
+    let errorLogger: ErrorLogger =
+        { new ErrorLogger("DiscardErrorsLogger") with
+            member x.DiagnosticSink(phasedError, severity) = ()
+            member x.ErrorCount = 0 }
+
+    let langVersion = Features.LanguageVersion.Default
+    let reportLibraryOnlyFeatures = false
+    let canSkipTrivia = false //(flags &&& FSharpLexerFlags.SkipTrivia) = FSharpLexerFlags.SkipTrivia
+    let isLightSyntaxOn = false // (flags &&& FSharpLexerFlags.LightSyntaxOn) = FSharpLexerFlags.LightSyntaxOn
+    let isCompiling = false // (flags &&& FSharpLexerFlags.Compiling) = FSharpLexerFlags.Compiling
+    let isCompilingFSharpCore = false // (flags &&& FSharpLexerFlags.CompilingFSharpCore) = FSharpLexerFlags.CompilingFSharpCore
+    let canUseLexFilter = true // No idea...  (flags &&& FSharpLexerFlags.UseLexFilter) = FSharpLexerFlags.UseLexFilter
+
+    let lexbuf =
+        UnicodeLexing.SourceTextAsLexbuf(reportLibraryOnlyFeatures, langVersion, text)
+
+    let lightStatus = LightSyntaxStatus(isLightSyntaxOn, true)
+
+    let lexargs =
+        mkLexargs (conditionalCompilationDefines, lightStatus, LexResourceManager(0), [], errorLogger, pathMap)
+
+    let lexargs = { lexargs with applyLineDirectives = isCompiling }
+
+    let getNextToken =
+        let lexer = Lexer.token lexargs canSkipTrivia
+
+        if canUseLexFilter then
+            let lexFilter =
+                LexFilter.LexFilter(lexargs.lightStatus, isCompilingFSharpCore, lexer, lexbuf)
+
+            (fun _ -> lexFilter.GetToken())
+        else
+            lexer
+
+    resetLexbufPos "" lexbuf
+
+    while not lexbuf.IsPastEndOfStream do
+        // ct.ThrowIfCancellationRequested ()
+        onToken (getNextToken lexbuf) lexbuf.LexemeRange
+
+//        let source = "let a = 0 // comment" |> SourceText.ofString
+//        let tokens = ResizeArray()
+//        lex  source [] (fun t _r -> tokens.Add(t)) PathMap.empty
+//        Assert.IsFalse(Seq.isEmpty tokens)
---
+++ src/Fantomas.FCS/ParseAndCheckInputs.fs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All Rights Reserved. See License.txt in the project root for license information.
+
+/// Contains logic to coordinate the parsing and checking of one or a group of files
+module internal FSharp.Compiler.ParseAndCheckInputs
+
+
+open Internal.Utilities.Library
+open FSharp.Compiler.IO
+
+let FSharpSigFileSuffixes = [".mli";".fsi"]
+
+let mlCompatSuffixes = [".mli";".ml"]
+
+let FSharpImplFileSuffixes = [".ml";".fs";".fsscript";".fsx"]
+
+let FSharpScriptFileSuffixes = [".fsscript";".fsx"]
+
+
+let CanonicalizeFilename filename =
+    let basic = FileSystemUtils.fileNameOfPath filename
+    String.capitalize (try FileSystemUtils.chopExtension basic with _ -> basic)
+
+let IsScript filename =
+    let lower = String.lowercase filename
+    FSharpScriptFileSuffixes |> List.exists (FileSystemUtils.checkSuffix lower)
