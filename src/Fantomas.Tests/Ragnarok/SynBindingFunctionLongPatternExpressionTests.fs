b20091f71fc0a51f1ea89dd4e26cb548da7f07fa
Josh DeGraw <18509575+josh-degraw@users.noreply.github.com>
2022-03-18 10:51:00 -0600

    Initial settings for strousrup bracket style (#2161)
    * Bare minimal changes for one test case.
    
    * Initial draft of ragnarok feature.
    
    * Add more syntax node where the case occurs.
    
    * Maybe multiline strings?
    
    * Highlight additional cases in SynBinding.
    
    * Add new setting for ragnarok mode.
    Take anonymous records in SynBinding into account.
    
    * Separate test case into multiple files. Implement SynBindingFunction and Expressions.
    
    * Add support for computation expressions.
    
    * Add support for Array/List in SynBinding value/function.
    
    * Exclude update records.
    
    * Synbinding function with return type.
    
    * Add additional tests for type members.
    
    * Add LetOrUseBang with Expression.
    
    * Add YieldOrReturn with Expression.
    
    * Add YieldOrReturnBang with Expression.
    
    * Add SynExprAndBang with Expression.
    
    * Format test files.
    
    * Add tests suffix
    
    * Add LongIdentSetExpression with Expression.
    
    * Add DotIndexedSet with Expression.
    
    * Add Set with Expression.
    
    * Add DotSet with Expression.
    
    * Add Lambda with Expression.
    
    * Add SynMatchClause with Expression.
    
    * Add SynTypeDefnSimpleRepr.Record NOTE: TESTS FAIL
    
    * Add SynTypeDefnSigRepr.Simple
    
    * Use correct helper function for Set and DotSet.
    
    * Work around edge case where match clause body needs to be further indented.
    
    * Add named arguments in application and new expressions.
    
    * Fix leftover issue
    
    * Add TODO comment
    
    * Fix failing test
    
    * Format files
    
    Co-authored-by: nojaf <florian.verdonck@outlook.com>

---
+++ src/Fantomas.Tests/Ragnarok/SynBindingFunctionLongPatternExpressionTests.fs
@@ -0,0 +1,518 @@
+﻿module Fantomas.Tests.Ragnarok.SynBindingFunctionLongPatternExpressionTests
+
+open NUnit.Framework
+open FsUnit
+open Fantomas.Tests.TestHelper
+
+let config =
+    { config with
+        MaxLineLength = 80
+        MultilineBlockBracketsOnSameColumn = true
+        Ragnarok = true }
+
+// TODO: conclude on what should happen here
+// This one feels very weird to have `= {` because the pattern is already multiline
+[<Test>]
+let ``synbinding function with record`` () =
+    formatSourceString
+        false
+        """
+let private addTaskToScheduler
+    (scheduler: IScheduler)
+    taskName
+    taskCron
+    prio
+    (task: unit -> unit)
+    groupName
+    =
+    { A = longTypeName
+      B = someOtherVariable
+      C = ziggyBarX }
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+let private addTaskToScheduler
+    (scheduler: IScheduler)
+    taskName
+    taskCron
+    prio
+    (task: unit -> unit)
+    groupName
+    =
+    {
+        A = longTypeName
+        B = someOtherVariable
+        C = ziggyBarX
+    }
+"""
+
+[<Test>]
+let ``synbinding function with update record`` () =
+    formatSourceString
+        false
+        """
+let private addTaskToScheduler
+    (scheduler: IScheduler)
+    taskName
+    taskCron
+    prio
+    (task: unit -> unit)
+    groupName
+    =
+    { astContext with IsInsideMatchClausePattern = true }
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+let private addTaskToScheduler
+    (scheduler: IScheduler)
+    taskName
+    taskCron
+    prio
+    (task: unit -> unit)
+    groupName
+    =
+    { astContext with
+        IsInsideMatchClausePattern = true
+    }
+"""
+
+
+[<Test>]
+let ``synbinding function with anonymous record`` () =
+    formatSourceString
+        false
+        """
+let private addTaskToScheduler
+    (scheduler: IScheduler)
+    taskName
+    taskCron
+    prio
+    (task: unit -> unit)
+    groupName
+    =
+    {| A = longTypeName
+       B = someOtherVariable
+       C = ziggyBarX |}
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+let private addTaskToScheduler
+    (scheduler: IScheduler)
+    taskName
+    taskCron
+    prio
+    (task: unit -> unit)
+    groupName
+    =
+    {|
+        A = longTypeName
+        B = someOtherVariable
+        C = ziggyBarX
+    |}
+"""
+
+[<Test>]
+let ``synbinding function with computation expression`` () =
+    formatSourceString
+        false
+        """
+let private addTaskToScheduler
+    (scheduler: IScheduler)
+    taskName
+    taskCron
+    prio
+    (task: unit -> unit)
+    groupName
+    =
+    task {
+        // some computation here
+        ()
+    }
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+let private addTaskToScheduler
+    (scheduler: IScheduler)
+    taskName
+    taskCron
+    prio
+    (task: unit -> unit)
+    groupName
+    =
+    task {
+        // some computation here
+        ()
+    }
+"""
+
+[<Test>]
+let ``synbinding function with list`` () =
+    formatSourceString
+        false
+        """
+let private addTaskToScheduler
+    (scheduler: IScheduler)
+    taskName
+    taskCron
+    prio
+    (task: unit -> unit)
+    groupName
+    =
+    [ itemOne
+      itemTwo
+      itemThree
+      itemFour
+      itemFive ]
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+let private addTaskToScheduler
+    (scheduler: IScheduler)
+    taskName
+    taskCron
+    prio
+    (task: unit -> unit)
+    groupName
+    =
+    [
+        itemOne
+        itemTwo
+        itemThree
+        itemFour
+        itemFive
+    ]
+"""
+
+[<Test>]
+let ``synbinding function with array`` () =
+    formatSourceString
+        false
+        """
+let private addTaskToScheduler
+    (scheduler: IScheduler)
+    taskName
+    taskCron
+    prio
+    (task: unit -> unit)
+    groupName
+    =
+    [| itemOne
+       itemTwo
+       itemThree
+       itemFour
+       itemFive |]
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+let private addTaskToScheduler
+    (scheduler: IScheduler)
+    taskName
+    taskCron
+    prio
+    (task: unit -> unit)
+    groupName
+    =
+    [|
+        itemOne
+        itemTwo
+        itemThree
+        itemFour
+        itemFive
+    |]
+"""
+
+
+[<Test>]
+let ``type member function with record instance`` () =
+    formatSourceString
+        false
+        """
+type Foo() =
+    member this.addTaskToScheduler
+        (scheduler: IScheduler)
+        taskName
+        taskCron
+        prio
+        (task: unit -> unit)
+        groupName
+        =
+        { A = longTypeName
+          B = someOtherVariable
+          C = ziggyBarX }
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+type Foo() =
+    member this.addTaskToScheduler
+        (scheduler: IScheduler)
+        taskName
+        taskCron
+        prio
+        (task: unit -> unit)
+        groupName
+        =
+        {
+            A = longTypeName
+            B = someOtherVariable
+            C = ziggyBarX
+        }
+"""
+
+[<Test>]
+let ``type member function with update record`` () =
+    formatSourceString
+        false
+        """
+type Foo() =
+    member this.addTaskToScheduler
+        (scheduler: IScheduler)
+        taskName
+        taskCron
+        prio
+        (task: unit -> unit)
+        groupName
+        = { astContext with IsInsideMatchClausePattern = true }
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+type Foo() =
+    member this.addTaskToScheduler
+        (scheduler: IScheduler)
+        taskName
+        taskCron
+        prio
+        (task: unit -> unit)
+        groupName
+        =
+        { astContext with
+            IsInsideMatchClausePattern = true
+        }
+"""
+
+[<Test>]
+let ``type member function with anonymous record instance`` () =
+    formatSourceString
+        false
+        """
+type Foo() =
+    member this.addTaskToScheduler
+        (scheduler: IScheduler)
+        taskName
+        taskCron
+        prio
+        (task: unit -> unit)
+        groupName
+        =
+        {| A = longTypeName
+           B = someOtherVariable
+           C = ziggyBarX |}
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+type Foo() =
+    member this.addTaskToScheduler
+        (scheduler: IScheduler)
+        taskName
+        taskCron
+        prio
+        (task: unit -> unit)
+        groupName
+        =
+        {|
+            A = longTypeName
+            B = someOtherVariable
+            C = ziggyBarX
+        |}
+"""
+
+[<Test>]
+let ``type member function with anonymous record instance struct`` () =
+    formatSourceString
+        false
+        """
+type Foo() =
+    member this.addTaskToScheduler
+        (scheduler: IScheduler)
+        taskName
+        taskCron
+        prio
+        (task: unit -> unit)
+        groupName
+        =
+           struct
+                {| A = longTypeName
+                   B = someOtherVariable
+                   C = ziggyBarX |}
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+type Foo() =
+    member this.addTaskToScheduler
+        (scheduler: IScheduler)
+        taskName
+        taskCron
+        prio
+        (task: unit -> unit)
+        groupName
+        =
+        struct {|
+            A = longTypeName
+            B = someOtherVariable
+            C = ziggyBarX
+        |}
+"""
+
+[<Test>]
+let ``type member function with computation expression`` () =
+    formatSourceString
+        false
+        """
+type Foo() =
+    member this.addTaskToScheduler
+        (scheduler: IScheduler)
+        taskName
+        taskCron
+        prio
+        (task: unit -> unit)
+        groupName
+        =
+        task {
+            // some computation here
+            ()
+        }
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+type Foo() =
+    member this.addTaskToScheduler
+        (scheduler: IScheduler)
+        taskName
+        taskCron
+        prio
+        (task: unit -> unit)
+        groupName
+        =
+        task {
+            // some computation here
+            ()
+        }
+"""
+
+[<Test>]
+let ``type member function with list`` () =
+    formatSourceString
+        false
+        """
+type Foo() =
+    member this.addTaskToScheduler
+        (scheduler: IScheduler)
+        taskName
+        taskCron
+        prio
+        (task: unit -> unit)
+        groupName
+        =
+        [ itemOne
+          itemTwo
+          itemThree
+          itemFour
+          itemFive ]
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+type Foo() =
+    member this.addTaskToScheduler
+        (scheduler: IScheduler)
+        taskName
+        taskCron
+        prio
+        (task: unit -> unit)
+        groupName
+        =
+        [
+            itemOne
+            itemTwo
+            itemThree
+            itemFour
+            itemFive
+        ]
+"""
+
+[<Test>]
+let ``type member function with array`` () =
+    formatSourceString
+        false
+        """
+type Foo() =
+    member this.addTaskToScheduler
+        (scheduler: IScheduler)
+        taskName
+        taskCron
+        prio
+        (task: unit -> unit)
+        groupName
+        =
+        [| itemOne
+           itemTwo
+           itemThree
+           itemFour
+           itemFive |]
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+type Foo() =
+    member this.addTaskToScheduler
+        (scheduler: IScheduler)
+        taskName
+        taskCron
+        prio
+        (task: unit -> unit)
+        groupName
+        =
+        [|
+            itemOne
+            itemTwo
+            itemThree
+            itemFour
+            itemFive
+        |]
+"""
