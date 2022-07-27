ebf222a91d593d583a123d21596c366c18cdeb42
mataha <mataha@users.noreply.github.com>
2022-07-27 16:17:17 +0200

    Remove unnecessary tests
    Signed-off-by: mataha <mataha@users.noreply.github.com>

--- src/Fantomas.Core.Tests/ShebangTests.fs
+++ src/Fantomas.Core.Tests/ShebangTests.fs
@@ -16,29 +16,3 @@ printfn "the best thing we've ever done"
 """
 
     formatSourceString false codeSnippet config |> should equal codeSnippet
-
-[<Test>]
-let ``should work if a shebang is on the first line, 2367`` () =
-    let codeSnippet =
-        """#!/usr/bin/env -S dotnet fsi
-// random licensing stuff
-
-open System
-
-printfn "you can't be waiting on this"
-"""
-
-    isValidFSharpCode false codeSnippet |> should equal true
-
-[<Test>]
-let ``should fail if a shebang is not on the first line, 2367`` () =
-    let codeSnippet =
-        """// random licensing stuff
-#!/usr/bin/env -S dotnet fsi
-
-open System
-
-printfn "perfect imperfection"
-"""
-
-    isValidFSharpCode false codeSnippet |> should equal false
