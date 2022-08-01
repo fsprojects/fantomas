e2eda798e15b9bf36fc2bbfc809c87fe23681fcc
yisusalanpng <45440937+yisusalanpng@users.noreply.github.com>
2022-08-01 07:33:32 -0500

    New docs page custom theme (WIP) (#2377)
    * - (WIP) Add new navbar, and some tweaks to sidebar.
    
    * New layout for fixing the contents overflow and height problems
    
    * - Bump to boostrap 5.0.2
    - Finish navbar with right sizes and colors
    - Clean up sidebar a little
    - Added collapsing mechanism (WIP)
    
    * - Made sidebar collapsible
    - Collapsing animations
    - TBD: sidebar is collapsing when interacting with items inside
    
    * - Add new boostrap 5.2 files
    - WIP Mobile first approach
    - Fix bug of sidebar collapsing when clicking items
    - Add target="_blank" to some urls
    - Change some hardcoded urls to fsdocs variables
    - Change navbar to be sticky
    - Refactor sidebar to use arrow functions
    
    * - Tweak size-break delimeter from md to sm
    - Some sidebar stylings
    - Content body custom white styling
    
    * - Almost complete design mockup
    - Changed content background white
    - Fixed min-height to vh, but still have issues with bigger pages :(
    - Sidebar with background when item collapsed and arrow icon
    - Made special navbar for mobile
    - Tweaked sidebar spacings and font-size
    - TBD: Icons with hover animations, improve collapsible animation, add
      hover effect to text on sidebar.

--- docs/.style/bootstrap/js/src/popover.js
+++ docs/.style/bootstrap/js/src/popover.js
@@ -1,168 +1,95 @@
 /**
  * --------------------------------------------------------------------------
- * Bootstrap (v5.0.2): popover.js
+ * Bootstrap (v5.2.0): popover.js
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
  * --------------------------------------------------------------------------
  */
 
 import { defineJQueryPlugin } from './util/index'
-import SelectorEngine from './dom/selector-engine'
 import Tooltip from './tooltip'
 
 /**
- * ------------------------------------------------------------------------
  * Constants
- * ------------------------------------------------------------------------
  */
 
 const NAME = 'popover'
-const DATA_KEY = 'bs.popover'
-const EVENT_KEY = `.${DATA_KEY}`
-const CLASS_PREFIX = 'bs-popover'
-const BSCLS_PREFIX_REGEX = new RegExp(`(^|\\s)${CLASS_PREFIX}\\S+`, 'g')
-
-const Default = {
-  ...Tooltip.Default,
-  placement: 'right',
-  offset: [0, 8],
-  trigger: 'click',
-  content: '',
-  template: '<div class="popover" role="tooltip">' +
-              '<div class="popover-arrow"></div>' +
-              '<h3 class="popover-header"></h3>' +
-              '<div class="popover-body"></div>' +
-            '</div>'
-}
-
-const DefaultType = {
-  ...Tooltip.DefaultType,
-  content: '(string|element|function)'
-}
-
-const Event = {
-  HIDE: `hide${EVENT_KEY}`,
-  HIDDEN: `hidden${EVENT_KEY}`,
-  SHOW: `show${EVENT_KEY}`,
-  SHOWN: `shown${EVENT_KEY}`,
-  INSERTED: `inserted${EVENT_KEY}`,
-  CLICK: `click${EVENT_KEY}`,
-  FOCUSIN: `focusin${EVENT_KEY}`,
-  FOCUSOUT: `focusout${EVENT_KEY}`,
-  MOUSEENTER: `mouseenter${EVENT_KEY}`,
-  MOUSELEAVE: `mouseleave${EVENT_KEY}`
-}
-
-const CLASS_NAME_FADE = 'fade'
-const CLASS_NAME_SHOW = 'show'
 
 const SELECTOR_TITLE = '.popover-header'
 const SELECTOR_CONTENT = '.popover-body'
 
+const Default = {
+  ...Tooltip.Default,
+  content: '',
+  offset: [0, 8],
+  placement: 'right',
+  template: '<div class="popover" role="tooltip">' +
+    '<div class="popover-arrow"></div>' +
+    '<h3 class="popover-header"></h3>' +
+    '<div class="popover-body"></div>' +
+    '</div>',
+  trigger: 'click'
+}
+
+const DefaultType = {
+  ...Tooltip.DefaultType,
+  content: '(null|string|element|function)'
+}
+
 /**
- * ------------------------------------------------------------------------
- * Class Definition
- * ------------------------------------------------------------------------
+ * Class definition
  */
 
 class Popover extends Tooltip {
   // Getters
-
   static get Default() {
     return Default
-  }
-
-  static get NAME() {
-    return NAME
-  }
-
-  static get Event() {
-    return Event
   }
 
   static get DefaultType() {
     return DefaultType
   }
 
+  static get NAME() {
+    return NAME
+  }
+
   // Overrides
-
-  isWithContent() {
-    return this.getTitle() || this._getContent()
-  }
-
-  getTipElement() {
-    if (this.tip) {
-      return this.tip
-    }
-
-    this.tip = super.getTipElement()
-
-    if (!this.getTitle()) {
-      SelectorEngine.findOne(SELECTOR_TITLE, this.tip).remove()
-    }
-
-    if (!this._getContent()) {
-      SelectorEngine.findOne(SELECTOR_CONTENT, this.tip).remove()
-    }
-
-    return this.tip
-  }
-
-  setContent() {
-    const tip = this.getTipElement()
-
-    // we use append for html objects to maintain js events
-    this.setElementContent(SelectorEngine.findOne(SELECTOR_TITLE, tip), this.getTitle())
-    let content = this._getContent()
-    if (typeof content === 'function') {
-      content = content.call(this._element)
-    }
-
-    this.setElementContent(SelectorEngine.findOne(SELECTOR_CONTENT, tip), content)
-
-    tip.classList.remove(CLASS_NAME_FADE, CLASS_NAME_SHOW)
+  _isWithContent() {
+    return this._getTitle() || this._getContent()
   }
 
   // Private
-
-  _addAttachmentClass(attachment) {
-    this.getTipElement().classList.add(`${CLASS_PREFIX}-${this.updateAttachment(attachment)}`)
-  }
-
-  _getContent() {
-    return this._element.getAttribute('data-bs-content') || this._config.content
-  }
-
-  _cleanTipClass() {
-    const tip = this.getTipElement()
-    const tabClass = tip.getAttribute('class').match(BSCLS_PREFIX_REGEX)
-    if (tabClass !== null && tabClass.length > 0) {
-      tabClass.map(token => token.trim())
-        .forEach(tClass => tip.classList.remove(tClass))
+  _getContentForTemplate() {
+    return {
+      [SELECTOR_TITLE]: this._getTitle(),
+      [SELECTOR_CONTENT]: this._getContent()
     }
   }
 
-  // Static
+  _getContent() {
+    return this._resolvePossibleFunction(this._config.content)
+  }
 
+  // Static
   static jQueryInterface(config) {
     return this.each(function () {
       const data = Popover.getOrCreateInstance(this, config)
 
-      if (typeof config === 'string') {
-        if (typeof data[config] === 'undefined') {
-          throw new TypeError(`No method named "${config}"`)
-        }
-
-        data[config]()
+      if (typeof config !== 'string') {
+        return
       }
+
+      if (typeof data[config] === 'undefined') {
+        throw new TypeError(`No method named "${config}"`)
+      }
+
+      data[config]()
     })
   }
 }
 
 /**
- * ------------------------------------------------------------------------
  * jQuery
- * ------------------------------------------------------------------------
- * add .Popover to jQuery only if jQuery is present
  */
 
 defineJQueryPlugin(Popover)
