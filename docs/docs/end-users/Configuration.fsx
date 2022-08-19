42a0654664d0dfff96f844d94d07b95d80736bf1
alanlomeli <45440937+yisusalanpng@users.noreply.github.com>
2022-08-18 19:48:26 -0500

    Separated sections

--- docs/docs/end-users/Configuration.fsx
+++ docs/docs/end-users/Configuration.fsx
@@ -18,7 +18,9 @@ commandline. <br>
 Your IDE should respect your settings, but the implementation of that is editor specific. Setting the configuration via 
 UI might be available depending on the IDE but do note that these features might not be up to 
 date nor work properly, therefore it's support and use is out of scope for this project. 
+*)
 
+(**
 ## Usage
 Inside .editorconfig you can specify the file extension and code location to be use per config:
 ```
@@ -34,15 +36,15 @@ fsharp_bar_before_discriminated_union_declaration = true
 fsharp_multiline_block_brackets_on_same_column = true
 fsharp_experimental_stroustrup_style = true
 ```
+*)
+
+(**
 ## Trying your settings via the online tool
 You can quickly try your settings via the <a href="https://fsprojects.github.io/fantomas-tools/#/fantomas/preview" target="_blank">online tool</a>. <br>
 <img src="{{root}}/online_tool_usage.gif" alt="drawing" width="100%"/>
-
 *)
 
-
 (**
-
 ## Auxiliary settings
 ### indent_size
 ` indent_size` has to be between 1 and 10.
@@ -53,7 +55,6 @@ The same indentation is ensured to be consistent in a source file.
 Default = 4.
 
 `defaultConfig`
-
 *)
 let input = """ 
 let inline selectRandom (f: _ []) =
