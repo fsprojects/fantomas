30f73bc1c805d0c09e3cb7a268b5f71cb0e7ef38
Florian Verdonck <florian.verdonck@outlook.com>
2022-08-29 10:36:14 +0200

    Minor tweaks to the configuration documentation. (#2451)

--- docs/content/fantomas-setting-icon.js
+++ docs/content/fantomas-setting-icon.js
@@ -1,35 +1,54 @@
-import { html } from 'https://cdn.skypack.dev/lit';
-import { component } from 'https://cdn.skypack.dev/haunted';
+import {html} from 'https://cdn.skypack.dev/lit';
+import {component} from 'https://cdn.skypack.dev/haunted';
 
 
-function FantomasSettingIcon({ tooltip, type }) {
+function FantomasSettingIcon({tooltip, type}) {
     let settingType
     switch (type) {
         case 'green':
-            settingType = {icon: "bi-check-circle-fill", color: "green-recommendation", tooltip: tooltip?tooltip:"This setting is good to use"}
+            settingType = {
+                icon: "bi-check-circle-fill",
+                color: "green-recommendation",
+                tooltip: tooltip ? tooltip : "It is ok to change the value of this setting."
+            }
             break;
         case 'orange':
-            settingType =  {icon:"bi-exclamation-circle-fill", color:"orange-recommendation", tooltip: tooltip?tooltip:"This setting is not recommended"}
+            settingType = {
+                icon: "bi-exclamation-circle-fill",
+                color: "orange-recommendation",
+                tooltip: tooltip ? tooltip : "Changing the default of this setting is not recommended."
+            }
             break;
         case 'red':
-            settingType = {icon: "bi-x-circle-fill", color:"red-recommendation", tooltip: tooltip?tooltip:"You shouldn't use this setting"}
+            settingType = {
+                icon: "bi-x-circle-fill",
+                color: "red-recommendation",
+                tooltip: tooltip ? tooltip : "You shouldn't use this setting."
+            }
             break;
+        default:
+            throw "The \"type\" can only be \"green\", \"orange\" or \"red\""
     }
-    return  html`<i class="bi ${settingType.icon} ${settingType.color} me-2"
-                    data-bs-toggle="tooltip" data-bs-custom-class="${type}-tooltip" data-bs-title="${settingType.tooltip}"></i>`;
+    return html`<i class="bi ${settingType.icon} ${settingType.color} me-2"
+                   data-bs-toggle="tooltip" data-bs-custom-class="${type}-tooltip"
+                   data-bs-title="${settingType.tooltip}"></i>`;
 }
 
-function FantomasSettingIconGResearch({ tooltip }) {
+function FantomasSettingIconGResearch({tooltip}) {
     const root = document.documentElement.dataset.root
     const safeTooltip =
         tooltip ? tooltip : "If you use one of these you should use all G-Research settings for consistency reasons";
 
-    return  html`<img class="gresearch-recommendation me-2" data-bs-toggle="tooltip" data-bs-custom-class="gresearch-tooltip"
+    return html`<img class="gresearch-recommendation me-2" data-bs-toggle="tooltip" data-bs-custom-class="gresearch-tooltip"
                    data-bs-title="${safeTooltip}" data-bs-custom-class="gresearch-tooltip" src="${root}/images/gresearch.svg" alt="G-Research logo"/>`;
 }
 
-customElements.define('fantomas-setting-icon', component(FantomasSettingIcon, { useShadowDOM: false,
-                                                                               observedAttributes: [ 'tooltip','type'] }));
+customElements.define('fantomas-setting-icon', component(FantomasSettingIcon, {
+    useShadowDOM: false,
+    observedAttributes: ['tooltip', 'type']
+}));
 
-customElements.define('fantomas-setting-icon-gresearch', component(FantomasSettingIconGResearch, { useShadowDOM: false,
-                                                                               observedAttributes: [ 'tooltip'] }));
+customElements.define('fantomas-setting-icon-gresearch', component(FantomasSettingIconGResearch, {
+    useShadowDOM: false,
+    observedAttributes: ['tooltip']
+}));
