81837e6a3777b5b38ba3a1946d54238bdac8c411
alanlomeli <45440937+yisusalanpng@users.noreply.github.com>
2022-09-09 00:47:40 -0500

    Change page layout to grid

--- docs/content/docs-setup.js
+++ docs/content/docs-setup.js
@@ -4,18 +4,11 @@ addEventListener('load', (event) => {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
-    // Handling of sidebar open/close
-    const sidebarWrapper = document.getElementById("sidebar-wrapper");
-    const mainContainer = document.getElementById("main-container");
-    document.querySelector("#toggle-mobile-menu").addEventListener("click", () => {
-        mainContainer.classList.remove('toggled');
-        sidebarWrapper.classList.toggle("d-none");
-    })
-    
-    document.querySelector("#close-mobile-menu").addEventListener("click", () => {
-        mainContainer.classList.add('toggled');
-        sidebarWrapper.classList.toggle("d-none");
-    });
+    // get body element
+    const body = document.getElementsByTagName('body')[0];
+document.querySelector("#toggle-mobile-menu").addEventListener("click", () => {
+    body.classList.toggle("open-menu");
+        });
 
     // Opening/collapsing menu items of sidebar depending on the current page section
     const currentPathName = window.location.pathname;
