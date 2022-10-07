c6ae6767bcd8590f8cc2cf9172ff075d9fc50167
Florian Verdonck <florian.verdonck@outlook.com>
2022-10-07 21:23:56 +0200

    Change style of base constructor calls. (#2555)
    * Change style of base constructor calls.
    
    * Add changelog entry for 5.1.0-alpha-005.

---
+++ src/Fantomas.Core.Tests/BaseConstructorTests.fs
@@ -0,0 +1,68 @@
+module Fantomas.Core.Tests.BaseConstructorTests
+
+open NUnit.Framework
+open FsUnit
+open Fantomas.Core.Tests.TestHelper
+
+[<Test>]
+let ``multiple base constructors in record, 2111`` () =
+    formatSourceString
+        false
+        """
+type UnhandledWebException =
+    inherit Exception
+
+    new(status: WebExceptionStatus, innerException: Exception) =
+        { inherit Exception(SPrintF1
+                                "Backend not prepared for this WebException with Status[%i]"
+                                (int status),
+                            innerException) }
+
+    new(info: SerializationInfo, context: StreamingContext) =
+        { inherit Exception(info, context) }
+"""
+        { config with MaxLineLength = 100 }
+    |> prepend newline
+    |> should
+        equal
+        """
+type UnhandledWebException =
+    inherit Exception
+
+    new(status: WebExceptionStatus, innerException: Exception) =
+        { inherit
+            Exception(
+                SPrintF1 "Backend not prepared for this WebException with Status[%i]" (int status),
+                innerException
+            ) }
+
+    new(info: SerializationInfo, context: StreamingContext) = { inherit Exception(info, context) }
+"""
+
+[<Test>]
+let ``single multiline base constructor, 2335`` () =
+    formatSourceString
+        false
+        """
+type FieldNotFoundException<'T>(obj:'T, field:string, specLink:string) =
+    inherit SwaggerSchemaParseException(
+        sprintf "Object MUST contain field `%s` (See %s for more details).\nObject:%A"
+            field specLink obj)
+"""
+        { config with
+            SpaceBeforeClassConstructor = true
+            MaxLineLength = 90 }
+    |> prepend newline
+    |> should
+        equal
+        """
+type FieldNotFoundException<'T> (obj: 'T, field: string, specLink: string) =
+    inherit
+        SwaggerSchemaParseException (
+            sprintf
+                "Object MUST contain field `%s` (See %s for more details).\nObject:%A"
+                field
+                specLink
+                obj
+        )
+"""
