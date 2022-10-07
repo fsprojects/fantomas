5da94701ae77c288ccb7a2a06af716b2d528ac3d
Florian Verdonck <florian.verdonck@outlook.com>
2022-10-07 20:57:53 +0200

    Multiline type annotations (#2547)
    * Change style of multiline type annotations.
    
    * Update long lambda format.
    
    * Change multiline record field declarations.
    
    * Change long SignatureParameter type annotation.
    
    * Avoid vanity alignment in type application.
    
    * Fix multiline return type in SynBinding.

--- src/Fantomas.Core.Tests/CommentTests.fs
+++ src/Fantomas.Core.Tests/CommentTests.fs
@@ -2412,11 +2412,8 @@ type ExprFolder<'State> =
         """
 /// A set of function parameters (visitor) for folding over expressions
 type ExprFolder<'State> =
-    { exprIntercept: ('State -> Expr -> 'State) (* noInterceptF *)
-        -> ('State -> Expr -> 'State)
-        -> 'State
-        -> Expr
-        -> 'State (* recurseF *)
+    { exprIntercept:
+        ('State -> Expr -> 'State) (* noInterceptF *) -> ('State -> Expr -> 'State) -> 'State -> Expr -> 'State (* recurseF *)
       valBindingSiteIntercept: 'State -> bool * Val -> 'State
       nonRecBindingsIntercept: 'State -> Binding -> 'State
       recBindingsIntercept: 'State -> Bindings -> 'State
