6d4874163e5d9b3e7f58396ae90129165a4ba856
nojaf <florian.verdonck@outlook.com>
2022-10-15 14:50:29 +0200

    Trivia inside the tree 🤯

--- src/Fantomas.Core/CodePrinter2.fs
+++ src/Fantomas.Core/CodePrinter2.fs
@@ -11,38 +11,52 @@ type SingleTokenNode with
 
     member x.AddEvent = !-x.Token
 
+let genTrivia (trivia: TriviaNode) =
+    match trivia.Content with
+    | CommentOnSingleLine comment -> !-comment +> sepNln
+
+let genNode<'n when 'n :> NodeBase> (n: 'n) (f: 'n -> Context -> Context) =
+    col sepNln n.ContentBefore genTrivia
+    +> f n
+    +> col sepNln n.ContentAfter genTrivia
+
 let genIdentListNode (iln: IdentListNode) =
-    col sepNone iln.Content (function
-        | IdentifierOrDot.Ident ident -> ident.AddEvent
-        | IdentifierOrDot.KnownDot _
-        | IdentifierOrDot.UnknownDot _ -> sepDot)
+    genNode iln (fun iln ->
+        col sepNone iln.Content (function
+            | IdentifierOrDot.Ident ident -> ident.AddEvent
+            | IdentifierOrDot.KnownDot _
+            | IdentifierOrDot.UnknownDot _ -> sepDot))
 
 let genExpr (e: Expr) =
-    match e with
-    | Expr.Constant c -> c.AddEvent
+    genNode e.Node (fun _ ->
+        match e with
+        | Expr.Constant c -> c.AddEvent)
 
 let genBinding (b: Binding) =
-    b.LeadingKeyword.AddEvent
-    +> sepSpace
-    +> b.FunctionName.AddEvent
-    +> sepSpace
-    +> col sepSpace b.Parameters (fun _ -> sepNone)
-    +> b.Equals.AddEvent
-    +> sepSpace
-    +> genExpr b.Expr
+    genNode b (fun b ->
+        b.LeadingKeyword.AddEvent
+        +> sepSpace
+        +> b.FunctionName.AddEvent
+        +> sepSpace
+        +> col sepSpace b.Parameters (fun _ -> sepNone)
+        +> b.Equals.AddEvent
+        +> sepSpace
+        +> genExpr b.Expr)
 
 let genModuleDecl (md: ModuleDecl) =
     match md with
     | ModuleDecl.TopLevelBinding b -> genBinding b
 
 let genModule (m: ModuleOrNamespace) =
-    onlyIf
-        m.IsNamed
-        (optSingle (fun (n: IdentNode) -> n.AddEvent +> sepSpace +> genIdentListNode m.Name) m.LeadingKeyword
-         +> sepNln
-         +> sepNln)
-    +> col sepNln m.Declarations genModuleDecl
+    genNode m (fun m ->
+        onlyIf
+            m.IsNamed
+            (optSingle (fun (n: IdentNode) -> n.AddEvent +> sepSpace +> genIdentListNode m.Name) m.LeadingKeyword
+             +> sepNln
+             +> sepNln)
+        +> col (sepNln +> sepNln) m.Declarations genModuleDecl)
 
-let genFile (oak: FileNode) =
-    col sepNln oak.ModulesOrNamespaces genModule
-    +> (fun ctx -> onlyIf ctx.Config.InsertFinalNewline sepNln ctx)
+let genFile (oak: Oak) =
+    genNode oak (fun oak ->
+        col sepNln oak.ModulesOrNamespaces genModule
+        +> (fun ctx -> onlyIf ctx.Config.InsertFinalNewline sepNln ctx))
