b18c99ceabadb1f643d80dfbc9f0070c56c09861
nojaf <florian.verdonck@outlook.com>
2022-11-24 09:39:08 +0100

    Restore version.

--- src/Fantomas.Core/Version.fs
+++ src/Fantomas.Core/Version.fs
@@ -2,11 +2,11 @@ module internal Fantomas.Core.Version
 
 open System.Reflection
 
-let fantomasVersion = lazy "maybe a minor, maybe a major"
-// lazy
-//     (let assembly = typeof<TriviaTypes.FsAstType>.Assembly
-//
-//      assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
-//      |> Option.ofObj
-//      |> Option.map (fun a -> a.InformationalVersion)
-//      |> Option.defaultValue (Assembly.GetExecutingAssembly().GetName().Version.ToString()))
+let fantomasVersion =
+    lazy
+        (let assembly = typeof<SyntaxOak.Node>.Assembly
+
+         assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+         |> Option.ofObj
+         |> Option.map (fun a -> a.InformationalVersion)
+         |> Option.defaultValue (Assembly.GetExecutingAssembly().GetName().Version.ToString()))
