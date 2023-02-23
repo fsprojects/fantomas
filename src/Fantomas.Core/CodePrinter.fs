36b7325045a811ce2228090c90d6a6e6cf5ec462
Josh DeGraw <18509575+josh-degraw@users.noreply.github.com>
2023-02-23 00:36:34 -0700

    Fix handling of AppExpr with a single stroustrup record (#2747)
    * Fix handling of AppExpr with a single stroustrup record
    
    * Add some more tests
    
    * Refactor to handle record applications after other args
    
    * Add specific settings for new feature
    
    * Fix rebase issue
    
    * Remove extra setting for stroustrup record application expressions
    
    * Fix rebase issues

--- src/Fantomas.Core/CodePrinter.fs
+++ src/Fantomas.Core/CodePrinter.fs
@@ -1178,6 +1178,31 @@ let genExpr (e: Expr) =
                 else
                     short ctx
 
+            | EndsWithSingleRecordApp ctx.Config (sequentialArgs: Expr list, recordOrAnonRecord) ->
+                // check if everything else beside the last array/list fits on one line
+                let singleLineTestExpr =
+                    genExpr node.FunctionExpr +> sepSpace +> col sepSpace sequentialArgs genExpr
+
+                let short =
+                    genExpr node.FunctionExpr
+                    +> sepSpace
+                    +> col sepSpace sequentialArgs genExpr
+                    +> onlyIfNot sequentialArgs.IsEmpty sepSpace
+                    +> genExpr recordOrAnonRecord
+
+                let long =
+                    genExpr node.FunctionExpr
+                    +> indent
+                    +> sepNln
+                    +> col sepNln sequentialArgs genExpr
+                    +> onlyIfNot sequentialArgs.IsEmpty sepNln
+                    +> genExpr recordOrAnonRecord
+                    +> unindent
+
+                if futureNlnCheck singleLineTestExpr ctx then
+                    long ctx
+                else
+                    short ctx
             | _ ->
                 let shortExpression =
                     let sep ctx =
@@ -1224,7 +1249,7 @@ let genExpr (e: Expr) =
                 clauseNode.WhenExpr
             +> sepSpace
             +> genSingleTextNodeWithSpaceSuffix sepSpace clauseNode.Arrow
-            +> indentSepNlnUnindentExprUnlessStroustrup genExpr clauseNode.BodyExpr
+            +> indentSepNlnUnindentUnlessStroustrup genExpr clauseNode.BodyExpr
             +> leaveNode clauseNode
 
         atCurrentColumn (
@@ -1810,7 +1835,7 @@ let genNamedArgumentExpr (node: ExprInfixAppNode) =
         genExpr node.LeftHandSide
         +> sepSpace
         +> genSingleTextNode node.Operator
-        +> indentSepNlnUnindentExprUnlessStroustrup (fun e -> sepSpace +> genExpr e) node.RightHandSide
+        +> indentSepNlnUnindentUnlessStroustrup (fun e -> sepSpace +> genExpr e) node.RightHandSide
 
     expressionFitsOnRestOfLine short long |> genNode node
 
@@ -2136,6 +2161,22 @@ let (|EndsWithSingleListApp|_|) (config: FormatConfig) (appNode: ExprAppNode) =
             match args with
             | [] -> None
             | [ Expr.ArrayOrList _ as singleList ] -> Some(otherArgs.Close(), singleList)
+            | arg :: args ->
+                otherArgs.Add(arg)
+                visit args
+
+        visit appNode.Arguments
+
+let (|EndsWithSingleRecordApp|_|) (config: FormatConfig) (appNode: ExprAppNode) =
+    if not config.IsStroustrupStyle then
+        None
+    else
+        let mutable otherArgs = ListCollector<Expr>()
+
+        let rec visit (args: Expr list) =
+            match args with
+            | [] -> None
+            | [ Expr.Record _ | Expr.AnonStructRecord _ as singleRecord ] -> Some(otherArgs.Close(), singleRecord)
             | arg :: args ->
                 otherArgs.Add(arg)
                 visit args
@@ -2671,7 +2712,7 @@ let genBinding (b: BindingNode) (ctx: Context) : Context =
                     let short = sepSpace +> body
 
                     let long =
-                        indentSepNlnUnindentExprUnlessStroustrup (fun e -> sepSpace +> genExpr e) b.Expr
+                        indentSepNlnUnindentUnlessStroustrup (fun e -> sepSpace +> genExpr e) b.Expr
 
                     isShortExpression ctx.Config.MaxFunctionBindingWidth short long
 
@@ -2760,9 +2801,7 @@ let genBinding (b: BindingNode) (ctx: Context) : Context =
             +> (fun ctx ->
                 let prefix = afterLetKeyword +> sepSpace +> genValueName +> genReturnType
                 let short = prefix +> genExpr b.Expr
-
-                let long = prefix +> indentSepNlnUnindentExprUnlessStroustrup genExpr b.Expr
-
+                let long = prefix +> indentSepNlnUnindentUnlessStroustrup genExpr b.Expr
                 isShortExpression ctx.Config.MaxValueBindingWidth short long ctx)
 
     genNode b binding ctx
