608a37137cd780368e37907317dfc4ed46abc073
Florian Verdonck <florian.verdonck@outlook.com>
2023-02-24 14:16:34 +0100

    Manually deserialize FormatDocumentResponse. (#2780)
    * Manually deserialize FormatDocumentResponse.
    
    * Apply suggestions from code review
    
    Co-authored-by: dawe <dawedawe@posteo.de>
    
    ---------
    
    Co-authored-by: dawe <dawedawe@posteo.de>

---
+++ src/Fantomas.Tests/FantomasServiceTests.fs
@@ -0,0 +1,31 @@
+﻿module Fantomas.CoreGlobalTool.Tests.FantomasServiceTests
+
+open System
+open System.IO
+open Fantomas.Client.Contracts
+open Fantomas.Client.LSPFantomasServiceTypes
+open Fantomas.Client.LSPFantomasService
+open NUnit.Framework
+
+let toucheFileAndFormat (path: string) (service: FantomasService) : FantomasResponse =
+    let content = File.ReadAllText path
+    let dirtyContent = String.Concat(content, "    ")
+    File.WriteAllText(path, dirtyContent)
+
+    let request: FormatDocumentRequest =
+        { SourceCode = dirtyContent
+          FilePath = path
+          Config = None
+          Cursor = None }
+
+    service.FormatDocumentAsync(request).Result
+
+[<Explicit "This is meant to troubleshoot local problems">]
+[<Test>]
+let ``locate fantomas tool`` () =
+    let service: FantomasService = new LSPFantomasService()
+
+    let response =
+        toucheFileAndFormat @"C:\Users\nojaf\Projects\fantomas\src\Fantomas.Core\FormatConfig.fs" service
+
+    Assert.AreEqual(int FantomasResponseCode.Formatted, response.Code)
