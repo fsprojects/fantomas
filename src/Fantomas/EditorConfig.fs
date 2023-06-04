20037f097c28e8b7f19869d98b74474f597adeec
Florian Verdonck <florian.verdonck@outlook.com>
2023-06-04 16:39:39 +0200

    Update editorconfig package (#2895)
    * Use EditorConfigCache.
    
    * Clean up EditorConfig.fs(i)

--- src/Fantomas/EditorConfig.fs
+++ src/Fantomas/EditorConfig.fs
@@ -2,6 +2,7 @@ module Fantomas.EditorConfig
 
 open System.Collections.Generic
 open System.ComponentModel
+open EditorConfig.Core
 open Fantomas.Core
 
 module Reflection =
@@ -15,7 +16,7 @@ module Reflection =
           DisplayName: string option
           Description: string option }
 
-    let inline private getCustomAttribute<'t, 'v when 't :> Attribute and 't: null>
+    let inline getCustomAttribute<'t, 'v when 't :> Attribute and 't: null>
         (projection: 't -> 'v)
         (property: PropertyInfo)
         : 'v option =
@@ -40,7 +41,7 @@ let toEditorConfigName value =
     value
     |> Seq.map (fun c ->
         if System.Char.IsUpper(c) then
-            sprintf "_%s" (c.ToString().ToLower())
+            $"_%s{c.ToString().ToLower()}"
         else
             c.ToString())
     |> String.concat ""
@@ -49,28 +50,28 @@ let toEditorConfigName value =
         if List.contains name supportedProperties then
             name
         else
-            sprintf "fsharp_%s" name
+            $"fsharp_%s{name}"
 
-let private getFantomasFields (fallbackConfig: FormatConfig) =
+let getFantomasFields (fallbackConfig: FormatConfig) =
     Reflection.getRecordFields fallbackConfig
     |> Array.map (fun (recordField, defaultValue) ->
         let editorConfigName = toEditorConfigName recordField.PropertyName
 
         (editorConfigName, defaultValue))
 
-let private (|Number|_|) (d: string) =
+let (|Number|_|) (d: string) =
     match System.Int32.TryParse(d) with
     | true, d -> Some(box d)
     | _ -> None
 
-let private (|MultilineFormatterType|_|) mft =
+let (|MultilineFormatterType|_|) mft =
     MultilineFormatterType.OfConfigString mft
 
-let private (|BracketStyle|_|) bs = MultilineBracketStyle.OfConfigString bs
+let (|BracketStyle|_|) bs = MultilineBracketStyle.OfConfigString bs
 
-let private (|EndOfLineStyle|_|) eol = EndOfLineStyle.OfConfigString eol
+let (|EndOfLineStyle|_|) eol = EndOfLineStyle.OfConfigString eol
 
-let private (|Boolean|_|) b =
+let (|Boolean|_|) b =
     if b = "true" then Some(box true)
     elif b = "false" then Some(box false)
     else None
@@ -102,18 +103,18 @@ let configToEditorConfig (config: FormatConfig) : string =
             |> Some
         | :? System.Int32 as i -> $"%s{toEditorConfigName recordField.PropertyName}=%d{i}" |> Some
         | :? MultilineFormatterType as mft ->
-            sprintf "%s=%s" (toEditorConfigName recordField.PropertyName) (MultilineFormatterType.ToConfigString mft)
+            $"%s{toEditorConfigName recordField.PropertyName}=%s{MultilineFormatterType.ToConfigString mft}"
             |> Some
         | :? EndOfLineStyle as eols ->
-            sprintf "%s=%s" (toEditorConfigName recordField.PropertyName) (EndOfLineStyle.ToConfigString eols)
+            $"%s{toEditorConfigName recordField.PropertyName}=%s{EndOfLineStyle.ToConfigString eols}"
             |> Some
         | _ -> None)
     |> String.concat "\n"
 
-let private editorConfigParser = EditorConfig.Core.EditorConfigParser()
+let editorConfigParser = EditorConfigParser(EditorConfigFileCache.GetOrCreate)
 
 let tryReadConfiguration (fsharpFile: string) : FormatConfig option =
-    let editorConfigSettings: EditorConfig.Core.FileConfiguration =
+    let editorConfigSettings: FileConfiguration =
         editorConfigParser.Parse(fileName = fsharpFile)
 
     if editorConfigSettings.Properties.Count = 0 then
