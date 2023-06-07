dc9cda67a76af710a2d9e4f0de57447f1cd3b299
nojaf <florian.verdonck@outlook.com>
2023-06-07 15:02:10 +0200

    Add cursor regression test.

--- src/Fantomas.Core.Tests/CursorTests.fs
+++ src/Fantomas.Core.Tests/CursorTests.fs
@@ -44,3 +44,30 @@ let ``cursor inside a node between defines`` () =
 """
         (3, 4)
     |> assertCursor (2, 0)
+
+[<Test>]
+let ``cursor after try keyword`` () =
+    formatWithCursor
+        """
+namespace JetBrains.ReSharper.Plugins.FSharp.Services.Formatter
+
+[<CodeCleanupModule>]
+type FSharpReformatCode(textControlManager: ITextControlManager) =
+        member x.Process(sourceFile, rangeMarker, _, _, _) =
+            if isNotNull rangeMarker then
+                try
+                    let range = ofDocumentRange rangeMarker.DocumentRange
+                    let formatted = fantomasHost.FormatSelection(filePath, range, text, settings, parsingOptions, newLineText)
+                    let offset = rangeMarker.DocumentRange.StartOffset.Offset
+                    let oldLength = rangeMarker.DocumentRange.Length
+                    let documentChange = DocumentChange(document, offset, oldLength, formatted, stamp, modificationSide)
+                    use _ = WriteLockCookie.Create()
+                    document.ChangeDocument(documentChange, TimeStamp.NextValue)
+                    sourceFile.GetPsiServices().Files.CommitAllDocuments()
+                with _ -> ()
+            else
+                let textControl = textControlManager.VisibleTextControls |> Seq.find (fun c -> c.Document == document)
+                cursorPosition = textControl.Caret.Position.Value.ToDocLineColumn();
+"""
+        (8, 19)
+    |> assertCursor (7, 15)
