09a5df0603409f5d6a10c75bf98dca9ed06c58dd
dawe <dawedawe@posteo.de>
2023-10-06 11:35:30 +0200

    build via sln

--- build.fsx
+++ build.fsx
@@ -77,7 +77,6 @@ pipeline "Build" {
         )
     }
     stage "CheckFormat" { run "dotnet fantomas src docs build.fsx --check" }
-    stage "GetAnalyzers" { run "dotnet build ./src/Fantomas.Analyzers/Fantomas.Analyzers.fsproj" }
     stage "Build" { run "dotnet build -c Release" }
     stage "UnitTests" { run "dotnet test -c Release" }
     stage "Pack" { run "dotnet pack --no-restore -c Release -o ./bin" }
