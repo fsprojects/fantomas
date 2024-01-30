50b257a2b323869650bab8280248733194fdfa7b
nojaf <florian.verdonck@outlook.com>
2024-01-30 17:20:01 +0100

    POC double indentation.

---
+++ src/Fantomas.Core.Tests/ExperimentalDoubleIdentParametersTests.fs
@@ -0,0 +1,30 @@
+﻿module Fantomas.Core.Tests.ExperimentalDoubleIdentParametersTests
+
+open NUnit.Framework
+open FsUnit
+open Fantomas.Core.Tests.TestHelpers
+
+let config =
+    { config with
+        ExperimentalDoubleIndentParameters = true
+        MaxLineLength = 10 }
+
+[<Test>]
+let ``initial sample`` () =
+    formatSourceString
+        """
+let sillyfuncWithParams parameterName1 ignoredParameterName2 ignoredParameterName3 =
+    longBody
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+let sillyfuncWithParams
+        parameterName1
+        ignoredParameterName2
+        ignoredParameterName3
+        =
+    longBody
+"""
