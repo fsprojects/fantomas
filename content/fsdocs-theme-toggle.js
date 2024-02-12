863545b5adcfe2ef24267200b9422f1c8dfc915e
nojaf <nojaf@users.noreply.github.com>
2024-02-12 10:25:46 +0000

    deploy: 85062f9b3820c263bffc0101ef17489faff3e64f

--- content/fsdocs-theme-toggle.js
+++ content/fsdocs-theme-toggle.js
@@ -2,9 +2,6 @@
 
 const prefersDark = window.matchMedia("@media (prefers-color-scheme: dark)").matches;
 let currentTheme = localStorage.getItem('theme') ?? (prefersDark ? 'dark' : 'light');
-if (currentTheme === 'dark') {
-    window.document.documentElement.setAttribute("data-theme", 'dark');
-}
 
 export class ThemeToggle extends LitElement {
     static properties = {
