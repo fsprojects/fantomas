b02aa511323ea8362f91aef9a20103ee7cb920e3
nojaf <florian.verdonck@outlook.com>
2024-09-13 10:24:19 +0200

    Add tests for new syntax

---
+++ src/Fantomas.Core.Tests/AutoPropertiesTests.fs
@@ -0,0 +1,37 @@
+﻿module Fantomas.Core.Tests.AutoPropertiesTests
+
+open NUnit.Framework
+open FsUnit
+open Fantomas.Core.Tests.TestHelpers
+
+[<Test>]
+let ``public get, private set`` () =
+    formatSourceString
+        """
+type X() =
+    member val Y: int = 7 with public get, private set
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+
+"""
+
+[<Test>]
+let ``public get, private set in signature`` () =
+    formatSignatureString
+        """
+module A
+
+type X() =
+    member val internal Y: int = 7 with public get, private set
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+
+"""
